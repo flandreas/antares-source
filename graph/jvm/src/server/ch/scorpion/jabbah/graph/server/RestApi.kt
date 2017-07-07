1b34fcd722db0c767047cc0997d216e79b863339
Andreas Fleischmann <andreas.fleischmann@swissconline.ch>
2017-07-07 19:38:31 +0200

    minor changes in RestApi

--- graph/jvm/src/server/ch/scorpion/jabbah/graph/server/RestApi.kt
+++ graph/jvm/src/server/ch/scorpion/jabbah/graph/server/RestApi.kt
@@ -7,18 +7,20 @@ import java.nio.file.Files
 import java.nio.file.Paths
 
 /**
- * The server that implements the REST API using sparc.
+ * The server that implements the REST API using spark.
  */
 class RestApi(val cmdLine: CommandLine) {
 
     companion object {
+
+    val LOG = LoggerFactory.getLogger(RestApi::class.java)
 
         @JvmStatic fun main(args: Array<String>) {
             val options = defineOptions()
             try {
                 RestApi(DefaultParser().parse(options, args))
             } catch (x: ParseException) {
-                System.out.println("Error while parsing options: '${x.message}'")
+                LOG.error("Error while parsing options: '${x.message}'")
                 HelpFormatter().printHelp("Jabbah REST API", options)
                 System.exit(1)
             }
@@ -38,11 +40,9 @@ class RestApi(val cmdLine: CommandLine) {
         }
     }
 
-    val LOG = LoggerFactory.getLogger(RestApi::class.java)
-
     init {
-        System.out.println("Jabbah REST API server started")
-        System.out.println("Accessing drawings in directory '${cmdLine.getOptionValue("d")}'")
+        LOG.info("Jabbah REST API server started")
+        LOG.info("Accessing drawings in directory '${cmdLine.getOptionValue("d")}'")
 
         get("/hello/:name") {req, res ->
             res.type("text/xml")
@@ -53,10 +53,10 @@ class RestApi(val cmdLine: CommandLine) {
     fun getDrawing(name: String): String? {
         val path = Paths.get(cmdLine.getOptionValue("d"), name)
         if (!Files.exists(path)) {
-            System.out.println("Path '$path' doesn't exist")
+            LOG.error("Path '$path' doesn't exist")
             return null
         }
-        System.out.println("Returning contents of '$path'")
+        LOG.info("Returning contents of '$path'")
         return String(Files.readAllBytes(path))
     }
 }
