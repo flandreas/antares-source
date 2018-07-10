f83f9c9a230fe3e09365954d35d78502cbf15409
Andreas Fleischmann <andreas.fleischmann@swissonline.ch>
2018-07-10 19:00:38 +0200

    allow to choose color in CircuitInOutViewBeanInfo

--- antares/jvm/src/main/ch/scorpion/antares/view/inout/CircuitInOutViewBeanInfo.kt
+++ antares/jvm/src/main/ch/scorpion/antares/view/inout/CircuitInOutViewBeanInfo.kt
@@ -2,18 +2,20 @@ package ch.scorpion.antares.view.inout
 
 import ch.scorpion.antares.model.signal.BitWidth
 import ch.scorpion.antares.model.signal.DigitalSignalRepresentation
-import com.l2fprod.common.propertysheet.Property
+import ch.scorpion.jabbah.base.geom.Direction
+import ch.scorpion.jabbah.draw.graphics.PredefinedColor
 import ch.scorpion.jabbah.edit.AbstractBeanInfo
 import ch.scorpion.jabbah.edit.Editor
 import ch.scorpion.jabbah.edit.PropertyImpl
 import ch.scorpion.jabbah.edit.model.text.TextProperty
-import ch.scorpion.jabbah.base.geom.Direction
 import ch.scorpion.jabbah.graph.model.PortType
+import com.l2fprod.common.propertysheet.Property
 
 
 /**
- * A [BeanInfo] for [CircuitInOutView].
+ * A [AbstractBeanInfo] for [CircuitInOutView].
  */
+@Suppress("unused")
 class CircuitInOutViewBeanInfo : AbstractBeanInfo<CircuitInOutView>() {
 
     companion object {
@@ -22,6 +24,7 @@ class CircuitInOutViewBeanInfo : AbstractBeanInfo<CircuitInOutView>() {
         private val portType = PropertyImpl("graph.property.portType", PortType::class.java)
         private val bitWidth = PropertyImpl("element.property.bitWidth", BitWidth::class.java)
         private val orientation = PropertyImpl("graph.property.direction", Direction::class.java)
+	    private val color = PropertyImpl("edit.property.color", PredefinedColor::class.java)
         private val signalRep = PropertyImpl("element.property.DigitalSignalRepresentation", DigitalSignalRepresentation::class.java)
 	    private val toggle = PropertyImpl("element.property.Switch.toggle", Boolean::class.java)
         private val description = PropertyImpl("edit.property.description", TextProperty::class.java)
@@ -34,7 +37,8 @@ class CircuitInOutViewBeanInfo : AbstractBeanInfo<CircuitInOutView>() {
 		name.bind(editor, { bean.name }, { bean.name = it })
 		portType.bind(editor, { bean.portType }, { bean.portType = it!! })
 		orientation.bind(editor, { bean.orientation }, { bean.orientation = it!! })
-		bitWidth.bind(editor, { bean.bitWidth }, { bean.bitWidth = it!! })
+	    color.bind(editor, { bean.customColor }, { bean.customColor = it })
+	    bitWidth.bind(editor, { bean.bitWidth }, { bean.bitWidth = it!! })
 		signalRep.bind(editor, { bean.signalRepresentation }, { bean.signalRepresentation = it!! })
 	    toggle.bind(editor, { bean.toggle }, { bean.toggle = it!! })
 		description.bind(editor, { bean.description}, { bean.description = it!! })
@@ -43,6 +47,7 @@ class CircuitInOutViewBeanInfo : AbstractBeanInfo<CircuitInOutView>() {
 		properties.add(name)
 		properties.add(portType)
 		properties.add(orientation)
+	    properties.add(color)
 		properties.add(bitWidth)
 		properties.add(signalRep)
 	    properties.add(toggle)
