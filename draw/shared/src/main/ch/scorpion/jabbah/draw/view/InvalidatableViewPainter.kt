cfa5f6738e143f7b9d6294e492a7813fce8f7c06
Andreas Fleischmann <andreas.fleischmann@swissonline.ch>
2018-09-28 15:45:51 +0200

    bug in repainting logic fixed
    If a Drawable invalidated the entire View, and a following operation
    invalidated a smaller region, that smaller region made it as the
    final region to be repainted. Now update the dirty region only
    if not the entire view has already been invalidated.

--- draw/shared/src/main/ch/scorpion/jabbah/draw/view/InvalidatableViewPainter.kt
+++ draw/shared/src/main/ch/scorpion/jabbah/draw/view/InvalidatableViewPainter.kt
@@ -20,22 +20,25 @@ class InvalidatableViewPainter(val view: View<*>) : ViewPainter {
         private val LOG by logger(InvalidatableViewPainter::class)
 
         /** The number of repaints per second.*/
-        private val REPAINT_FREQUENCY = 40
+        private const val REPAINT_FREQUENCY = 40
     }
 
     /** Keeps track of the current accumulated invalid region in model coordinate space.*/
     var dirtyRegion: Rectangle2D? = null
         private set
 
+	/** If set, indicates that the entire [View] is dirty, in which case [dirtyRegion] is overwritten.*/
+	private var dirtyView: Boolean = false
+
     private val timer: Timer = System.get().createTimer()
 
     init {
-        timer.initialize(1000 / REPAINT_FREQUENCY, {
-            timer.stop()
-            System.SYSTEM!!.invokeLater {
-                repaintDirtyRegion()
-            }
-        })
+        timer.initialize(1000 / REPAINT_FREQUENCY) {
+	        timer.stop()
+	        System.SYSTEM!!.invokeLater {
+		        repaintDirtyRegion()
+	        }
+        }
     }
 
     /** ---- [ViewPainter] interface */
@@ -50,10 +53,12 @@ class InvalidatableViewPainter(val view: View<*>) : ViewPainter {
 
     override fun invalidateRegion(region: RectangularShape?, ghost: Boolean) {
         if (region == null) {
-            dirtyRegion = null
+	        dirtyView = true
             RepaintingObserver.invalidated(Rectangle2D(0, 0, view.width, view.height))
         } else {
-            dirtyRegion = dirtyRegion?.add(region) as Rectangle2D? ?: Rectangle2D(region)
+	        if (!dirtyView) {
+		        dirtyRegion = dirtyRegion?.add(region) as Rectangle2D? ?: Rectangle2D(region)
+	        }
             RepaintingObserver.invalidated(region)
         }
     }
@@ -61,14 +66,19 @@ class InvalidatableViewPainter(val view: View<*>) : ViewPainter {
     /** ---- [InvalidatableViewPainter] */
 
     private fun repaintDirtyRegion() {
-            val p1 = if (dirtyRegion != null) view.modelToView(Point2D(dirtyRegion!!.minX, dirtyRegion!!.minY)) else Point2D(0, 0)
-            val p2 = if (dirtyRegion != null) view.modelToView(Point2D(dirtyRegion!!.maxX, dirtyRegion!!.maxY)) else Point2D(view.width, view.height)
-            val x1 = Math.floor(p1.x).toInt()
-            val y1 = Math.floor(p1.y).toInt()
-            val x2 = Math.ceil(p2.x).toInt()
-            val y2 = Math.ceil(p2.y).toInt()
-            view.repaint(x1 - 1, y1 - 1, x2 - x1 + 2, y2 - y1 + 2)
-            dirtyRegion = null
+        if (dirtyView) {
+	        view.repaint(0, 0, view.width, view.height)
+        } else {
+	        val p1 = if (dirtyRegion != null) view.modelToView(Point2D(dirtyRegion!!.minX, dirtyRegion!!.minY)) else Point2D(0, 0)
+	        val p2 = if (dirtyRegion != null) view.modelToView(Point2D(dirtyRegion!!.maxX, dirtyRegion!!.maxY)) else Point2D(view.width, view.height)
+	        val x1 = Math.floor(p1.x).toInt()
+	        val y1 = Math.floor(p1.y).toInt()
+	        val x2 = Math.ceil(p2.x).toInt()
+	        val y2 = Math.ceil(p2.y).toInt()
+	        view.repaint(x1 - 1, y1 - 1, x2 - x1 + 2, y2 - y1 + 2)
+        }
+        dirtyRegion = null
+	    dirtyView = false
     }
 
     private fun startTimerIfNeeded() {
