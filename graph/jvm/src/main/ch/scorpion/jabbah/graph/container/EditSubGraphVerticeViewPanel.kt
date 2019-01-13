0bb300ffb9ab84b10b654b3306b948e90c9bfd44
Andreas Fleischmann <andreas.fleischmann@swissonline.ch>
2019-01-13 10:44:04 +0100

    added menu bar to custom container editor

--- graph/jvm/src/main/ch/scorpion/jabbah/graph/container/EditSubGraphVerticeViewPanel.kt
+++ graph/jvm/src/main/ch/scorpion/jabbah/graph/container/EditSubGraphVerticeViewPanel.kt
@@ -1,7 +1,12 @@
 package ch.scorpion.jabbah.graph.container
 
+import ch.scorpion.jabbah.base.AbstractAction
+import ch.scorpion.jabbah.base.ActionWrapperSwing
 import ch.scorpion.jabbah.base.Translations
+import ch.scorpion.jabbah.base.event.ActionEvent
+import ch.scorpion.jabbah.base.invocation.BusyHandler
 import ch.scorpion.jabbah.base.logger
+import ch.scorpion.jabbah.edit.app.DeleteAction
 import ch.scorpion.jabbah.graph.MetaGraphRepository
 import ch.scorpion.jabbah.graph.model.module.GraphModelModule
 import ch.scorpion.jabbah.graph.view.vertice.SubGraphVerticeView
@@ -10,9 +15,9 @@ import ch.scorpion.jabbah.io.StorableCloner
 import java.awt.BorderLayout
 import java.awt.Dimension
 import java.awt.Frame
-import javax.swing.BoxLayout
-import javax.swing.JOptionPane
-import javax.swing.JPanel
+import java.awt.event.WindowAdapter
+import java.awt.event.WindowEvent
+import javax.swing.*
 
 
 /**
@@ -23,7 +28,8 @@ class EditSubGraphVerticeViewPanel(
 	private val metaGraphRepository: MetaGraphRepository = GraphModelModule.metaGraphRepository,
     private val containerPanel: ContainerPanel,
     private val subGraphVerticeView: SubGraphVerticeView<*>,
-    private val storableCloner: StorableCloner = IOModule.storableClonerProvider.invoke()
+    private val storableCloner: StorableCloner = IOModule.storableClonerProvider.invoke(),
+	private val closeHandler: (Boolean) -> Unit
 ) : JPanel() {
 
 	companion object {
@@ -41,14 +47,39 @@ class EditSubGraphVerticeViewPanel(
 			subGraphVerticeView: SubGraphVerticeView<*>,
 			storableCloner: StorableCloner
 		): Boolean {
-			val panel = EditSubGraphVerticeViewPanel(metaGraphRepository, containerPanel, subGraphVerticeView, storableCloner)
-			return JOptionPane.showConfirmDialog(
-				parent,
-				panel,
-				Translations.getString("graph.action.editSubGraphVerticeView.name"),
-				JOptionPane.OK_CANCEL_OPTION,
-				JOptionPane.PLAIN_MESSAGE
-			) == JOptionPane.OK_OPTION
+			val dialog = JDialog(parent, true)
+			var okPressed = false
+			BusyHandler.register(dialog, null)
+
+			dialog.title = Translations.getString("graph.action.editSubGraphVerticeView.name")
+			dialog.rootPane.jMenuBar = createMenuBar()
+			dialog.contentPane.add(EditSubGraphVerticeViewPanel(
+				metaGraphRepository,
+				containerPanel,
+				subGraphVerticeView,
+				storableCloner
+			) { okPressed = it
+				dialog.isVisible = false
+				dialog.dispose()
+			})
+			dialog.pack()
+			dialog.setLocationRelativeTo(parent)
+			dialog.addWindowListener(object : WindowAdapter() {
+				override fun windowClosed(e: WindowEvent?) {
+					BusyHandler.deregister(dialog)
+				}
+			})
+			dialog.isVisible = true
+
+			return okPressed
+		}
+
+		private fun createMenuBar(): JMenuBar {
+			val menuBar = JMenuBar()
+			val editMenu = JMenu(Translations.getString("application.menu.edit"))
+			menuBar.add(editMenu)
+			editMenu.add(JMenuItem(ActionWrapperSwing(DeleteAction())))
+			return menuBar
 		}
 	}
 
@@ -69,6 +100,7 @@ class EditSubGraphVerticeViewPanel(
 
 	    add(createToolBarPanel(), BorderLayout.NORTH)
         add(containerPanel, BorderLayout.CENTER)
+	    add(createButtonPanel(), BorderLayout.SOUTH)
     }
 
     private fun fill() {
@@ -83,5 +115,27 @@ class EditSubGraphVerticeViewPanel(
 		toolbarPanel.layout = BoxLayout(toolbarPanel, BoxLayout.LINE_AXIS)
 		containerPanel.createToolbars(separator = false).forEach { toolbarPanel.add(it) }
 		return toolbarPanel
+	}
+
+	private fun createButtonPanel(): JPanel {
+		val panel = JPanel()
+		panel.layout = BoxLayout(panel, BoxLayout.LINE_AXIS)
+		panel.add(Box.createHorizontalGlue())
+		panel.add(JButton(ActionWrapperSwing(CancelAction())))
+		panel.add(JButton(ActionWrapperSwing(OKAction())))
+		panel.border = BorderFactory.createEmptyBorder(10, 10, 10, 10)
+		return panel
+	}
+
+	private inner class OKAction : AbstractAction("base.action.ok") {
+		override fun execute(event: ActionEvent) {
+			closeHandler.invoke(true)
+		}
+	}
+
+	private inner class CancelAction : AbstractAction("base.action.cancel") {
+		override fun execute(event: ActionEvent) {
+			closeHandler.invoke(false)
+		}
 	}
 }
