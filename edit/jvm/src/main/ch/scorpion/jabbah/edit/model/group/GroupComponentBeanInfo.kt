bfc2917752134d01f8b47269ac6b46f26eff53cb
Andreas Fleischmann <andreas.fleischmann@swissonline.ch>
2019-01-18 09:40:20 +0100

    added missing GroupComponentBeanInfo

---
+++ edit/jvm/src/main/ch/scorpion/jabbah/edit/model/group/GroupComponentBeanInfo.kt
@@ -0,0 +1,6 @@
+package ch.scorpion.jabbah.edit.model.group
+
+import ch.scorpion.jabbah.edit.ComponentBeanInfo
+
+@Suppress("unused")
+class GroupComponentBeanInfo : ComponentBeanInfo<GroupComponent>()
