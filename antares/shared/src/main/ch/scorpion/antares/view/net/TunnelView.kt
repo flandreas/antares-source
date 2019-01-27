a81d8a497efd29a291d62922371ad27a9081be8a
Andreas Fleischmann <andreas.fleischmann@swissonline.ch>
2019-01-27 09:33:24 +0100

    improved TunnelView
    - changed look
    - PortViews ends at border to avoid misplacement of BitWidthAnnotation

--- antares/shared/src/main/ch/scorpion/antares/view/net/TunnelView.kt
+++ antares/shared/src/main/ch/scorpion/antares/view/net/TunnelView.kt
@@ -25,109 +25,115 @@ import ch.scorpion.jabbah.graph.GraphApplicationContext
  * A view representation of a [Tunnel].
  */
 class TunnelView(
-        styleProvider: StyleProvider = DrawStyleModule.styleProvider,
-        model: Tunnel = Tunnel()
+	styleProvider: StyleProvider = DrawStyleModule.styleProvider,
+	model: Tunnel = Tunnel()
 ) : DigitalComponentView<Tunnel>(styleProvider, model) {
 
-    companion object {
-        const val SIZE = 4 * 7
-        const val LABEL_DIST = Look.SCALE
-    }
+	companion object {
+		const val SIZE = 4 * 7
+		const val LABEL_DIST = Look.SCALE
+	}
 
-    private val label = HorizontalLabel(
-            owner = this,
-            relLocation = Point2D(SIZE / 2 + (DigitalPortView.LENGTH + SIZE / 2) + LABEL_DIST, 0),
-            font = font
-    )
+	private val label = HorizontalLabel(
+		owner = this,
+		relLocation = Point2D(SIZE / 2 + (DigitalPortView.LENGTH + SIZE / 2) + LABEL_DIST, 0),
+		font = font
+	)
 
-    init {
-        modelExchanged(null)
-        setBounds(getInput().unconnectedLength - SIZE / 2, -SIZE / 2, SIZE, SIZE)
-    }
+	init {
+		modelExchanged(null)
+		setBounds(getInput().unconnectedLength, -SIZE / 2, SIZE, SIZE)
+	}
 
-    override fun modelExchanged(oldModel: Tunnel?) {
-        super.modelExchanged(oldModel)
-        val portView = DigitalPortView(
-            styleProvider = styleProvider,
-            port = model!!.getPort(),
-            direction = Direction.WEST,
-            length = DigitalPortView.LENGTH + SIZE / 2,
-            predefinedConnectedLength = SIZE / 2)
-        portView.setLocation(portView.length.toDouble(), 0.0)
-        addPortView(portView)
+	override fun modelExchanged(oldModel: Tunnel?) {
+		super.modelExchanged(oldModel)
+		val portView = DigitalPortView(
+			styleProvider = styleProvider,
+			port = model!!.getPort(),
+			direction = Direction.WEST)
+		portView.setLocation(portView.length.toDouble(), 0.0)
+		addPortView(portView)
 
-        updateLabel()
-    }
+		updateLabel()
+	}
 
-    /** ---- UI properties */
+	/** ---- UI properties */
 
-    var name: String?
-        get() = model!!.name
-        set(value) {
-            model!!.name = value
-            updateLabel()
-        }
+	var name: String?
+		get() = model!!.name
+		set(value) {
+			model!!.name = value
+			updateLabel()
+		}
 
-    var bitWidth: BitWidth
-        get() = model!!.bitWidth
-        set(value) {
-            invalidate()
-            model!!.bitWidth = value
-            invalidate()
-        }
+	var bitWidth: BitWidth
+		get() = model!!.bitWidth
+		set(value) {
+			invalidate()
+			model!!.bitWidth = value
+			invalidate()
+		}
 
-    /** ---- [AbstractDrawable] */
+	/** ---- [AbstractDrawable] */
 
-    override fun draw(context: DrawContext) {
-        super.draw(context)
-        context.g.color = context.choose(styleProvider.getStyle(StyleType.BACKGROUND).color).textColor
-        label.draw(context)
-    }
+	override fun draw(context: DrawContext) {
+		super.draw(context)
+		context.g.color = context.choose(styleProvider.getStyle(StyleType.BACKGROUND).color).textColor
+		label.draw(context)
+	}
 
-    override fun drawImpl(context: DrawContext) {
+	override fun drawImpl(context: DrawContext) {
 		if (shadow) {
 			DropShadow.draw(context, transparency) {
 				context.g.fillRect(xInt, yInt, SIZE, SIZE)
 			}
 		}
 
-        context.g.color = context.choose(color).backgroundColor
-        context.g.fillRect(xInt, yInt, SIZE, SIZE)
+		context.g.color = context.choose(color).backgroundColor
+		context.g.fillRect(xInt, yInt, SIZE, SIZE)
 
-        context.g.color = context.choose(color).foregroundColor
-        context.g.stroke = stroke
-        context.g.drawRect(xInt, yInt, SIZE, SIZE)
+		context.g.color = context.choose(color).foregroundColor
+		context.g.stroke = stroke
+		context.g.drawRect(xInt, yInt, SIZE, SIZE)
 
-        // Draw the PortView above the border
-        super.drawImpl(context)
+		// Draw the PortView above the border
+		super.drawImpl(context)
 
-        if (ApplicationMode.EXECUTE == context.castedAppContext<GraphApplicationContext>()!!.mode) {
-            context.g.color = model!!.getInOrOutSignal().getColor().foregroundColor
-        }
-        context.g.fillOval(xInt + SIZE / 4, yInt + SIZE / 4, SIZE / 2, SIZE / 2)
-    }
+		if (ApplicationMode.EXECUTE == context.castedAppContext<GraphApplicationContext>()!!.mode) {
+			context.g.color = model!!.getInOrOutSignal().getColor().foregroundColor
+		}
 
-    override val boundingBox: Rectangle2D
-        get() {
-            val bb = super.boundingBox
-            val lbb = label.boundingBox.moveBy(location)
-            bb.add(lbb)
-            return bb
-        }
+		// Draw tunnel entry
+		context.g.translate(xInt + SIZE / 2.0, 0.0)
+		context.g.rotate(rotation.inverse().angle)
+		context.g.fillOval(-SIZE / 4, -SIZE / 4, SIZE / 2, SIZE / 2)
+		context.g.fillRect(-SIZE / 4, 0, SIZE / 2, SIZE / 4)
 
-    /** ---- [AbstractComponent] */
+		context.g.rotate(-rotation.inverse().angle)
+		context.g.translate(-(xInt + SIZE / 2.0), 0.0)
+	}
 
-    override fun rotationChanged(newRotation: Rotation) {
-        super.rotationChanged(newRotation)
-        label.rotationChanged()
-    }
+	override val boundingBox: Rectangle2D
+		get() {
+			val bb = super.boundingBox
+			val lbb = label.boundingBox.moveBy(location)
+			bb.add(lbb)
+			return bb
+		}
 
-    /** ---- [TunnelView] */
+	/** ---- [AbstractComponent] */
 
-    private fun updateLabel() {
-        invalidate()
-        label.text = StringUtils.orEmpty(name)
-        label.rotationChanged()
-        invalidate()
-    }
+	override fun rotationChanged(newRotation: Rotation) {
+		super.rotationChanged(newRotation)
+		label.rotationChanged()
+	}
+
+	/** ---- [TunnelView] */
+
+	private fun updateLabel() {
+		invalidate()
+		label.text = StringUtils.orEmpty(name)
+		label.rotationChanged()
+		invalidate()
+	}
 }
