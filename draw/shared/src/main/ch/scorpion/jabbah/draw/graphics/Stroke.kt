e63c58802367f969b8394f1afeefa718a46a559c
Andreas Fleischmann <andreas.fleischmann@swissonline.ch>
2019-07-31 21:03:29 +0200

    RectangularBelowSelectionModel is drawn stoked instead of filled
    This is to better support unfilled rectangled

--- draw/shared/src/main/ch/scorpion/jabbah/draw/graphics/Stroke.kt
+++ draw/shared/src/main/ch/scorpion/jabbah/draw/graphics/Stroke.kt
@@ -3,53 +3,52 @@ package ch.scorpion.jabbah.draw.graphics
 import ch.scorpion.jabbah.base.exception.IllegalArgumentException
 
 enum class LineCap {
-    BUTT,
-    ROUND,
-    SQUARE
+	BUTT,
+	ROUND,
+	SQUARE
 }
 
 enum class LineJoin {
-    MITER,
-    ROUND,
-    BEVEL
-
+	MITER,
+	ROUND,
+	BEVEL
 }
 
 /**
  * An implementation of the [Stroke] interface mainly to be used for targeting the JavaScript platform.
  */
 data class Stroke(
-        val width: Float = 1.0f,
-        val cap: LineCap = LineCap.SQUARE,
-        val join: LineJoin = LineJoin.MITER,
-        val miterLimit: Float = 10.0f,
-        val dash: FloatArray? = null,
-        val dashPhase: Float? = null)
-{
-    init {
-        if (width < 0.0) {
-            throw IllegalArgumentException("negative width")
-        }
-        if (join == LineJoin.MITER && miterLimit < 1.0) {
-            throw IllegalArgumentException("miterlimit < 1")
-        }
-        if (dash != null) {
-            if (dashPhase != null && dashPhase < 0.0) {
-                throw IllegalArgumentException("negative dash phase")
-            }
-            var allZero = false
-            for (value in dash) {
-                if (value > 0.0) {
-                    allZero = false
-                } else if (value < 0.0) {
-                    throw IllegalArgumentException("negative dash length")
-                }
-            }
-            if (allZero) {
-                throw IllegalArgumentException("dash lengths all zero")
-            }
-        }
-    }
+	val width: Float = 1.0f,
+	val cap: LineCap = LineCap.SQUARE,
+	val join: LineJoin = LineJoin.MITER,
+	val miterLimit: Float = 10.0f,
+	val dash: FloatArray? = null,
+	val dashPhase: Float? = null) {
+
+	init {
+		if (width < 0.0) {
+			throw IllegalArgumentException("negative width")
+		}
+		if (join == LineJoin.MITER && miterLimit < 1.0) {
+			throw IllegalArgumentException("miterlimit < 1")
+		}
+		if (dash != null) {
+			if (dashPhase != null && dashPhase < 0.0) {
+				throw IllegalArgumentException("negative dash phase")
+			}
+			var allZero = false
+			for (value in dash) {
+				if (value > 0.0) {
+					allZero = false
+				} else if (value < 0.0) {
+					throw IllegalArgumentException("negative dash length")
+				}
+			}
+			if (allZero) {
+				throw IllegalArgumentException("dash lengths all zero")
+			}
+		}
+	}
 }
 
 
--- edit/shared/src/main/ch/scorpion/jabbah/edit/model/rectangle/RectangularBelowSelectionModel.kt
+++ edit/shared/src/main/ch/scorpion/jabbah/edit/model/rectangle/RectangularBelowSelectionModel.kt
@@ -3,6 +3,7 @@ package ch.scorpion.jabbah.edit.model.rectangle
 import ch.scorpion.jabbah.base.geom.Rectangle2D
 import ch.scorpion.jabbah.draw.DrawContext
 import ch.scorpion.jabbah.draw.drawable.AbstractDrawable
+import ch.scorpion.jabbah.draw.graphics.Stroke
 import ch.scorpion.jabbah.draw.style.Themes
 import ch.scorpion.jabbah.edit.SelectionDrawingStrategy
 import ch.scorpion.jabbah.edit.SelectionModel
@@ -16,37 +17,40 @@ class RectangularBelowSelectionModel(
 	component: AbstractRectangularComponent
 ) : AbstractSelectionModel<AbstractRectangularComponent>(component) {
 
-    companion object {
-        private const val OUTSET: Int = 5
-    }
+	companion object {
+		private const val WITH_HALF: Int = 5
+		private const val WITH = 2 * WITH_HALF
+		private val STROKE = Stroke(WITH.toFloat())
+	}
 
-    private val bounds: Rectangle2D = Rectangle2D()
+	private val bounds: Rectangle2D = Rectangle2D()
 
-    /** ---- [AbstractDrawable] */
+	/** ---- [AbstractDrawable] */
 
-    override val boundingBox: Rectangle2D
-        get() = Rectangle2D(bounds.x - 1, bounds.y - 1, bounds.width + 2, bounds.height + 2)
+	override val boundingBox: Rectangle2D
+		get() = Rectangle2D(bounds.x - WITH_HALF - 1, bounds.y - WITH_HALF - 1, bounds.width + WITH + 1, bounds.height + WITH + 1)
 
-    override fun draw(context: DrawContext) {
-        context.g.color = Themes.get<EditTheme>().selection.color.foregroundColor
-        context.g.fillRect(bounds.x.toInt(), bounds.y.toInt(), bounds.width.toInt(), bounds.height.toInt())
-    }
+	override fun draw(context: DrawContext) {
+		context.g.stroke = STROKE
+		context.g.color = Themes.get<EditTheme>().selection.color.foregroundColor
+		context.g.draw(bounds)
+	}
 
-    override fun contains(x: Double, y: Double): Boolean {
-        return bounds.contains(x, y)
-    }
+	override fun contains(x: Double, y: Double): Boolean {
+		return bounds.contains(x, y)
+	}
 
-    /** ---- [AbstractSelectionModel] */
+	/** ---- [AbstractSelectionModel] */
 
-    override fun componentUpdated() {
-        invalidate()
-        bounds.setFrame(
-                component.shape.x - OUTSET,
-                component.shape.y - OUTSET,
-                component.shape.width + 2 * OUTSET,
-                component.shape.height + 2 * OUTSET
-        )
-        invalidate()
-        validate()
-    }
+	override fun componentUpdated() {
+		invalidate()
+		bounds.setFrame(
+			component.shape.x,
+			component.shape.y,
+			component.shape.width,
+			component.shape.height
+		)
+		invalidate()
+		validate()
+	}
 }
