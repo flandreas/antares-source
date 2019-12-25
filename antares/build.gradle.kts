e332f75b79916adcc743ca933febfe66f5a91941
Andreas Fleischmann <andreas.fleischmann@swissonline.ch>
2019-12-25 19:27:08 +0100

    added "shadowJar", "run" and "deploy" tasks to antares

--- antares/build.gradle.kts
+++ antares/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 val mockkVersion: String by extra
 val slf4jVersion: String by extra
 val commonsIoVersion: String by extra
@@ -6,6 +8,7 @@ val l2fprodVersion: String by extra
 
 plugins {
 	kotlin("multiplatform")
+	id("com.github.johnrengelman.shadow") version "5.1.0"
 }
 
 kotlin {
@@ -81,3 +84,54 @@ kotlin {
 		}
 	}
 }
+
+tasks {
+	val shadowCreate by creating(ShadowJar::class) {
+		manifest {
+			attributes["Main-Class"] = "ch.scorpion.antares.AntaresSwing"
+		}
+		archiveClassifier.set("all")
+		from(kotlin.jvm().compilations.getByName("main").output)
+		configurations =
+			mutableListOf(kotlin.jvm().compilations.getByName("main").compileDependencyFiles as Configuration)
+	}
+
+	val build by existing {
+		dependsOn(shadowCreate)
+	}
+
+	val run by creating(JavaExec::class) {
+		dependsOn(shadowCreate)
+		classpath = files("build/libs/antares-all.jar")
+		main = "ch.scorpion.antares.AntaresSwing"
+	}
+
+	val deploy by creating(Exec::class) {
+		dependsOn(assemble)
+
+		val version = file("shared/rsc/version.txt").readText().trim()
+		val nativeType = when {
+			System.getProperty("os.name").toLowerCase().contains("windows") -> "msi"
+			System.getProperty("os.name").toLowerCase().contains("mac") -> "pkg"
+			System.getProperty("os.name").toLowerCase().contains("linux") -> "rpm"
+			else -> throw IllegalStateException("unknown system")
+		}
+
+		workingDir = projectDir
+		commandLine(
+			"javapackager",
+			"-deploy",
+			"-nosign",
+			"-native", nativeType,
+			"-outdir", "${buildDir}/distributions",
+			"-outfile", project.name,
+			"-name", "Antares",
+			"-appclass", "ch.scorpion.antares.AntaresSwing",
+			"-srcdir", "build/libs",
+			"-srcfiles", "antares-all.jar",
+			"-BappVersion=${version}",
+			"-Bicon=jvm/rsc/antares.icns"
+		)
+	}
+}
+
