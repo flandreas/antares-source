379d92e628ffc0754309b2d2b539da19beb4763a
Andreas Fleischmann <andreas.fleischmann@swissonline.ch>
2020-04-04 11:24:43 +0200

    avoid using desktop.Taskbar when on Windows

--- antares/jvm/src/main/ch/scorpion/antares/AntaresSwing.kt
+++ antares/jvm/src/main/ch/scorpion/antares/AntaresSwing.kt
@@ -34,6 +34,7 @@ import org.apache.commons.cli.CommandLine
 import org.apache.commons.cli.Option
 import org.apache.commons.cli.Options
 import org.apache.commons.io.IOUtils
+import org.apache.commons.lang3.SystemUtils
 import java.awt.*
 import java.nio.file.Files
 import java.nio.file.Paths
@@ -108,7 +109,9 @@ class AntaresSwing(
 
 	init {
 
-		Taskbar.getTaskbar().iconImage = Toolkit.getDefaultToolkit().getImage(AntaresSwing::class.java.classLoader.getResource("img/Logo64.png"))
+		if (SystemUtils.IS_OS_MAC) {
+			Taskbar.getTaskbar().iconImage = Toolkit.getDefaultToolkit().getImage(AntaresSwing::class.java.classLoader.getResource("img/Logo64.png"))
+		}
 
 		eventBus.register(OpenMemoryContentsRequest::class) { request ->
 			if (request.newDesktopView) {
