d463737b9ee3fc0c007133ca6bda554ec25a5d6b
Andreas Fleischmann <andreas.fleischmann@swissonline.ch>
2020-04-16 09:29:49 +0200

    display error dialog in OpenContainerLibraryElementAction when loading fails

--- graph/jvm/src/main/ch/scorpion/jabbah/graph/library/OpenContainerLibraryElementAction.kt
+++ graph/jvm/src/main/ch/scorpion/jabbah/graph/library/OpenContainerLibraryElementAction.kt
@@ -3,9 +3,13 @@ package ch.scorpion.jabbah.graph.library
 import ch.scorpion.jabbah.app.DesktopApplication
 import ch.scorpion.jabbah.app.Savable
 import ch.scorpion.jabbah.base.Action
+import ch.scorpion.jabbah.base.Translations
 import ch.scorpion.jabbah.base.event.EventBus
+import ch.scorpion.jabbah.base.logger
 import ch.scorpion.jabbah.edit.model.ComponentMessage
 import ch.scorpion.jabbah.edit.model.ComponentMessageType
+import javax.swing.JFrame
+import javax.swing.JOptionPane
 
 /**
  * An [Action] for opening the [ContainerLibraryElement] that is currently selected in the
@@ -16,6 +20,10 @@ class OpenContainerLibraryElementAction(
 	libraryTreeView: LibraryTreeView,
 	eventBus: EventBus
 ) : AbstractContainerLibraryElementAction("graph.action.openContainerLibraryElement", libraryTreeView, eventBus) {
+
+	companion object {
+		private val LOG by logger(OpenContainerLibraryElementAction::class)
+	}
 
 	init {
 		eventBus.register(OpenContainerLibraryElementRequest::class) {
@@ -43,8 +51,18 @@ class OpenContainerLibraryElementAction(
 	}
 
 	private fun openAsSavable(element: ContainerLibraryElement) {
-		val library = element.library!!
-		library.libraryService.loadMetaGraph(library, element)
-		application.open(element.metaGraph!!, library.createSavable(element))
+		try {
+			val library = element.library!!
+			library.libraryService.loadMetaGraph(library, element)
+			application.open(element.metaGraph!!, library.createSavable(element))
+		} catch (e: Throwable) {
+			LOG.error("Error while loading ${element.uuid}: ${e.message}")
+			JOptionPane.showConfirmDialog(
+				JFrame.getFrames()[0],
+				Translations.getString("graph.action.load.error.general.desc"),
+				name,
+				JOptionPane.DEFAULT_OPTION,
+				JOptionPane.ERROR_MESSAGE)
+		}
 	}
 }
