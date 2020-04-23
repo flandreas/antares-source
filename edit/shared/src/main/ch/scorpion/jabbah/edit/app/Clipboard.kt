5825fff4abee64475ce123ddf969932d7d9c728c
Andreas Fleischmann <andreas.fleischmann@swissonline.ch>
2020-04-23 19:28:05 +0200

    refactored cut/copy/paste system - eliminated utility classes, replace by services - consolidated various action classes

--- edit/shared/src/main/ch/scorpion/jabbah/edit/app/Clipboard.kt
+++ edit/shared/src/main/ch/scorpion/jabbah/edit/app/Clipboard.kt
@@ -3,82 +3,90 @@ package ch.scorpion.jabbah.edit.app
 import ch.scorpion.jabbah.base.event.ActionEvent
 import ch.scorpion.jabbah.base.event.EventBus
 import ch.scorpion.jabbah.base.module.BaseModule
-import ch.scorpion.jabbah.draw.view.AbstractViewAction
 import ch.scorpion.jabbah.draw.view.DrawViewModule
 import ch.scorpion.jabbah.draw.view.ViewManager
-import ch.scorpion.jabbah.edit.CommandManager
 import ch.scorpion.jabbah.edit.Component
 import ch.scorpion.jabbah.edit.Drawing
 import ch.scorpion.jabbah.edit.DrawingView
+import ch.scorpion.jabbah.edit.command.AbstractCommand
+import ch.scorpion.jabbah.edit.model.CopyPasteService
 import ch.scorpion.jabbah.edit.module.EditModule
-import ch.scorpion.jabbah.io.IOModule
-import ch.scorpion.jabbah.io.StorableCreator
-import ch.scorpion.jabbah.io.TypeMap
 
-interface CopyPasteUtility {
+expect object Clipboard {
 
-	/** Cuts the specified [Component]s from the [Drawing] of the specified [DrawingView] to the system clipboard.*/
-	fun cut(
-		view: DrawingView<Drawing<Component>>,
-		components: Collection<Component>,
-		typeMap: TypeMap = IOModule.typeMap,
-		commandManager: CommandManager = EditModule.commandManager
-	)
+	fun getStringContents(): String?
 
-	/** Copies the specified [Component]s to the system clipboard.*/
-	fun copy(
-		drawing: Drawing<*>,
-		components: Collection<Component>,
-		typeMap: TypeMap = IOModule.typeMap
-	)
-
-	/** Pastes the contents of the system clipboard into the [Drawing] of the specified [DrawingView].*/
-	fun paste(
-		view: DrawingView<Drawing<Component>>,
-		storableCreator: StorableCreator = IOModule.storableCreator,
-		typeMap: TypeMap = IOModule.typeMap,
-		commandManager: CommandManager = EditModule.commandManager
-	)
+	fun setStringContents(contents: String)
 }
 
 class CutAction(
-	private val copyPasteUtility: CopyPasteUtility = EditModule.copyPasteUtility,
+	private val service: DrawingAppService = EditModule.drawingAppService,
 	eventBus: EventBus = BaseModule.eventBus,
-	viewManager: ViewManager = DrawViewModule.viewManager,
-	private val typeMap: TypeMap = IOModule.typeMap,
-	private val commandManager: CommandManager = EditModule.commandManager
+	viewManager: ViewManager = DrawViewModule.viewManager
 ) : AbstractSelectionAwareAction("edit.action.cut", eventBus, viewManager) {
 
 	override fun execute(event: ActionEvent) {
-		val drawingView = viewManager.castedActiveView<DrawingView<Drawing<Component>>>()!!
-		copyPasteUtility.cut(drawingView, drawingView.selectionManager.selection, typeMap, commandManager)
+		service.cut(drawingView!! as DrawingView<Drawing<Component>>)
 	}
 }
 
 class CopyAction(
-	private val copyPasteUtility: CopyPasteUtility = EditModule.copyPasteUtility,
+	private val service: DrawingAppService = EditModule.drawingAppService,
 	eventBus: EventBus = BaseModule.eventBus,
-	viewManager: ViewManager = DrawViewModule.viewManager,
-	private val typeMap: TypeMap = IOModule.typeMap
+	viewManager: ViewManager = DrawViewModule.viewManager
 ) : AbstractSelectionAwareAction("edit.action.copy", eventBus, viewManager) {
 
 	override fun execute(event: ActionEvent) {
-		val drawingView = viewManager.activeView as DrawingView<*>
-		copyPasteUtility.copy(drawingView.drawing, drawingView.selectionManager.selection, typeMap)
+		service.copy(drawingView!! as DrawingView<Drawing<Component>>)
 	}
 }
 
 class PasteAction(
-	private val copyPasteUtility: CopyPasteUtility = EditModule.copyPasteUtility,
+	private val service: DrawingAppService = EditModule.drawingAppService,
 	eventBus: EventBus = BaseModule.eventBus,
-	viewManager: ViewManager = DrawViewModule.viewManager,
-	private val storableCreator: StorableCreator = IOModule.storableCreator,
-	private val typeMap: TypeMap = IOModule.typeMap,
-	private val commandManager: CommandManager = EditModule.commandManager
+	viewManager: ViewManager = DrawViewModule.viewManager
 ) : AbstractEditAction("edit.action.paste", eventBus, viewManager) {
 
 	override fun execute(event: ActionEvent) {
-		val drawingView = viewManager.castedActiveView<DrawingView<Drawing<Component>>>()!!
-		copyPasteUtility.paste(drawingView, storableCreator, typeMap, commandManager)
+		service.paste(drawingView!! as DrawingView<Drawing<Component>>)
 	}
 }
+
+class CopyCommand(
+	private val drawingView: DrawingView<Drawing<Component>>,
+	private val componentIds: Collection<Int>,
+	private val service: CopyPasteService = EditModule.copyPasteService
+) : AbstractCommand("edit.command.copy") {
+
+	var oldContent: String? = null
+
+	override fun execute() {
+		oldContent = Clipboard.getStringContents()
+		Clipboard.setStringContents(service.copy(componentIds, drawingView.drawing))
+	}
+
+	override fun undo() {
+		oldContent?.let { Clipboard.setStringContents(it) }
+	}
+}
+
+/**
+ * Adds the previously copied [Component]s to a [Drawing].
+ */
+class PasteCommand(
+	private val drawingView: DrawingView<Drawing<Component>>,
+	private val clipboardContents: String,
+	private val service: CopyPasteService = EditModule.copyPasteService
+) : AbstractCommand("edit.command.paste", null) {
+
+	lateinit var components: Collection<Component>
+
+	override fun execute() {
+		components = service.paste(clipboardContents, drawingView)
+	}
+
+	override fun undo() {
+		components.forEach { drawingView.drawing.remove(it) }
+	}
+}
+
