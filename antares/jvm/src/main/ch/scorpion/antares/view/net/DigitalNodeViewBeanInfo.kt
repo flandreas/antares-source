34f74a846ff4307e87c06e94abb860f429c6cb2b
Andreas Fleischmann <andreas.fleischmann@swissonline.ch>
2021-05-19 14:52:45 +0200

    added dummy BeanInfo for OriginIndicator and DigitalNodeView
    used to avoid errors in LOG files

---
+++ antares/jvm/src/main/ch/scorpion/antares/view/net/DigitalNodeViewBeanInfo.kt
@@ -0,0 +1,7 @@
+package ch.scorpion.antares.view.net
+
+import ch.scorpion.jabbah.edit.properties.AbstractBeanInfo
+
+/** Has no properties by intention. */
+@Suppress("unused")
+class DigitalNodeViewBeanInfo : AbstractBeanInfo<DigitalNodeView>()
---
+++ graph/jvm/src/main/ch/scorpion/jabbah/graph/container/OriginIndicatorBeanInfo.kt
@@ -0,0 +1,7 @@
+package ch.scorpion.jabbah.graph.container
+
+import ch.scorpion.jabbah.edit.properties.AbstractBeanInfo
+
+/** Has no properties by intention. */
+@Suppress("unused")
+class OriginIndicatorBeanInfo : AbstractBeanInfo<OriginIndicator>()
