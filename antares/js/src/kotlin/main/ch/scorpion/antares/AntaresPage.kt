f016533bb053615d6678afa3e2b98a3c6cbf86d8
Andreas Fleischmann <andreas.fleischmann@swissonline.ch>
2021-08-23 20:07:48 +0200

    JS: added more content to AntaresPage

--- antares/js/src/kotlin/main/ch/scorpion/antares/AntaresPage.kt
+++ antares/js/src/kotlin/main/ch/scorpion/antares/AntaresPage.kt
@@ -22,7 +22,7 @@ import kotlinx.css.backgroundColor
 import react.dom.h2
 import react.dom.p
 import react.dom.render
-import styled.css
+import styled.*
 
 /** A React application displaying possible multiple [GraphViewerJs].*/
 class AntaresPage {
@@ -57,33 +57,106 @@ class AntaresPage {
 			mContainer(maxWidth = Breakpoint.md) {
 				css {
 					backgroundColor = Color.white
-					//backgroundColor = kotlinx.css.Color("#dcedfa")
 				}
-				//h2 { +"This is AntaresPage" }
 				mTypography("Binary Addition", variant = MTypographyVariant.h3)
 
-				//p { +"This is a circuit from the project"  }
-				mTypography("This is a circuit from the project", paragraph = true)
+				mTypography("""
+					|To start thinking about addition in binary terms, let's look at the binary addition
+					|of two 1-bit values - the simplest form of addition we can imagine.
+					""".trimMargin(),
+					paragraph = true)
+
+				styledTable {
+					styledTr {
+						styledTh { mTypography("Addition") }
+						styledTh { mTypography("Result") }
+						styledTh { mTypography("Carry") }
+					}
+					styledTr {
+						styledTd { mTypography("0 + 0") }
+						styledTd { mTypography("0") }
+						styledTd { mTypography("0") }
+					}
+					styledTr {
+						styledTd { mTypography("0 + 1") }
+						styledTd { mTypography("1") }
+						styledTd { mTypography("0") }
+					}
+					styledTr {
+						styledTd { mTypography("1 + 0") }
+						styledTd { mTypography("1") }
+						styledTd { mTypography("0") }
+					}
+					styledTr {
+						styledTd { mTypography("1 + 1") }
+						styledTd { mTypography("0") }
+						styledTd { mTypography("1") }
+					}
+				}
+
+				mTypography("""
+					|Let's first look at calculating the 'Result' value. You notice immediately that the result value
+					|is 1 only if the two added values are different. This is exactly what is the calculation result
+					|of an XOR gate.
+					""".trimMargin(),
+					paragraph = true)
+
+				mTypography("""
+					|Play around with the XOR gate in the circuit below and try different input values.
+					""".trimMargin(),
+					paragraph = true)
+
 				graphViewer {
 					canvasId = "canvas0"
 					metaGraphUuid = UUID("440b10dc-0999-4426-aa0f-c22c5221f641")
 					size = Dimension2D(500, 400)
 				}
 
-				//p { +"The following explains how a 'Half Adder' works."  }
-				mTypography("The following explains how a 'Half Adder' works.", paragraph = true)
+				mTypography("""
+					|Now let's look at calculating the 'Carry' value. The truth table tells you that the carry value
+					|is 1 only if both added values are 1. This is exactly what is the calculation result
+					|of an AND gate.
+					""".trimMargin(),
+					paragraph = true)
+
 				graphViewer {
 					canvasId = "canvas1"
+					metaGraphUuid = UUID("ae8e1d4c-5201-489d-9494-8eec9f54380d")
+					size = Dimension2D(500, 400)
+				}
+
+				mTypography("""
+					|Now let's combine the calculation of the result bit and the carry into a single circuit.
+					|We call this combined circuit "Half Adder". It produces a "Sum" (or result) bit and a "Carry" bit.
+					""".trimMargin(),
+					paragraph = true)
+
+				graphViewer {
+					canvasId = "canvas2"
 					metaGraphUuid = UUID("52255dc4-c010-4f6f-8ea6-9c2c8f5f9a82")
 					size = Dimension2D(500, 400)
 				}
 
-				//p { +"The following explains how a 'Full Adder' works."  }
-				mTypography("The following explains how a 'Full Adder' works.", paragraph = true)
+				mTypography("""
+					|Up to now, we can only add 2 bits. If you want to add numbers consisting of multiple bits,
+					|you add the two corresponding bits of each number, but you also have to respect the carry
+					|bit from the previous addition. This is done by a circuit called "Full Adder", which consists
+					|of two "Half Adders". The second "Half Adder" adds the result of the addition to the carry
+					|of the previous addition.
+					|
+					""".trimMargin(),
+					paragraph = true)
+
 				graphViewer {
-					canvasId = "canvas2"
+					canvasId = "canvas3"
 					metaGraphUuid = UUID("08aba425-96c2-4c43-b10b-2e0c72ce8300")
 					size = Dimension2D(500, 400)
+				}
+
+				graphViewer {
+					canvasId = "canvas4"
+					metaGraphUuid = UUID("e2252451-b870-4541-ac11-1ffe2aca72ca")
+					size = Dimension2D(800, 400)
 				}
 			}
 		}
