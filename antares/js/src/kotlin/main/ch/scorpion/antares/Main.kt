8343580b0c1d52f3503014196e6ba83af89a4595
Andreas Fleischmann <andreas.fleischmann@swissonline.ch>
2021-09-08 10:51:22 +0200

    JS: fixed main() to support iframe on GitHub jekyll page

--- antares/js/src/kotlin/main/ch/scorpion/antares/Main.kt
+++ antares/js/src/kotlin/main/ch/scorpion/antares/Main.kt
@@ -3,10 +3,12 @@ package ch.scorpion.antares
 external fun encodeURI(uri: String): String
 
 fun main() {
-	when (kotlinx.browser.window.location.pathname) {
-		encodeURI("/"), encodeURI("/index.html") -> AntaresPage().show()
-		encodeURI("/iframe.html") -> AntaresIFrame().show()
+	val path = kotlinx.browser.window.location.pathname
+	println("Path: $path")
+	when (path) {
+		encodeURI("/iframe.html"), encodeURI("/docs/web/iframe.html") -> AntaresIFrame().show()
 		encodeURI("/desktop.html") -> AntaresJs().start()
+		else -> AntaresPage().show()
 	}
 }
 
