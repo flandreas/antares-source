a701e7b915990d8a8086fd741d8c664dc1ff99b6
Andreas Fleischmann <andreas.fleischmann@swissonline.ch>
2021-10-26 09:53:38 +0200

    refactoring: extracted TransistorViewRenderer from TransistorView

--- antares/shared/src/main/ch/scorpion/antares/view/net/TransistorView.kt
+++ antares/shared/src/main/ch/scorpion/antares/view/net/TransistorView.kt
@@ -12,7 +12,6 @@ import ch.scorpion.antares.view.Look.SCALE
 import ch.scorpion.antares.view.port.DigitalPortView
 import ch.scorpion.jabbah.base.Properties
 import ch.scorpion.jabbah.base.StringUtils
-import ch.scorpion.jabbah.base.System
 import ch.scorpion.jabbah.base.geom.Direction
 import ch.scorpion.jabbah.base.geom.Direction.*
 import ch.scorpion.jabbah.base.geom.Point2D
@@ -21,15 +20,11 @@ import ch.scorpion.jabbah.base.geom.Rotation
 import ch.scorpion.jabbah.base.module.BaseModule
 import ch.scorpion.jabbah.draw.DrawContext
 import ch.scorpion.jabbah.draw.drawable.AbstractDrawable
-import ch.scorpion.jabbah.draw.graphics.LineCap
-import ch.scorpion.jabbah.draw.graphics.LineJoin
-import ch.scorpion.jabbah.draw.graphics.Stroke
 import ch.scorpion.jabbah.draw.style.DrawStyleModule
 import ch.scorpion.jabbah.draw.style.StyleProvider
 import ch.scorpion.jabbah.draw.style.StyleType
 import ch.scorpion.jabbah.edit.model.AbstractComponent
 import ch.scorpion.jabbah.edit.model.text.HorizontalLabel
-import ch.scorpion.jabbah.graph.GraphApplicationContext
 import ch.scorpion.jabbah.graph.view.port.PortLabelPosition
 import ch.scorpion.jabbah.graph.view.vertice.AbstractVerticeView
 import ch.scorpion.jabbah.io.Storable
@@ -46,36 +41,10 @@ class TransistorView(
 		/** The name of the [Boolean] property in [Properties] defining whether transistors are drawn with a circle. */
 		const val PROP_TRANSISTOR_CIRCLE = "antares.transistor.circle"
 
-		private val BAR_STROKE = Stroke(1.5f, LineCap.BUTT, LineJoin.ROUND)
 		private const val LABEL_DIST = SCALE
 		private val DEFAULT_HANDEDNESS = RIGHT
 		private const val WIDTH = 6 * SCALE
 		private const val HEIGHT = 6 * SCALE
-		private const val GATE_LINE_X = DigitalPortView.LENGTH + 2.0 * SCALE
-		private const val SIGNAL_LINE_X = DigitalPortView.LENGTH + 3.0 * SCALE
-		private const val SIGNAL_PORT_X = DigitalPortView.LENGTH + 4.0 * SCALE
-
-		private val N_ARROW_PATH = System.createPath()
-			.moveTo(SIGNAL_LINE_X, -2.0 * SCALE)
-			.lineTo(SIGNAL_PORT_X, -2.4 * SCALE)
-			.lineTo(SIGNAL_PORT_X, -1.6 * SCALE)
-			.close()
-
-		private val P_ARROW_PATH = System.createPath()
-			.moveTo(DigitalPortView.LENGTH + 5.0 * SCALE, -2.0 * SCALE)
-			.lineTo(SIGNAL_PORT_X, -2.4 * SCALE)
-			.lineTo(SIGNAL_PORT_X, -1.6 * SCALE)
-			.close()
-
-		private val SOUTH_SIGNAL_PATH = System.createPath()
-			.moveTo(SIGNAL_LINE_X, -0.5 * SCALE)
-			.lineTo(SIGNAL_PORT_X, -0.5 * SCALE)
-			.lineTo(SIGNAL_PORT_X, 1.0 * SCALE)
-
-		private val NORTH_SIGNAL_PATH = System.createPath()
-			.moveTo(SIGNAL_LINE_X, -3.5 * SCALE)
-			.lineTo(SIGNAL_PORT_X, -3.5 * SCALE)
-			.lineTo(SIGNAL_PORT_X, -5.0 * SCALE)
 
 		private val hasCircle: Boolean get() = BaseModule.properties.getBoolean(PROP_TRANSISTOR_CIRCLE)
 	}
@@ -148,47 +117,6 @@ class TransistorView(
 		updateGeometry()
 	}
 
-	private fun updateGeometry() {
-		getPortView(model.getGatePort())?.apply {
-			setLocation(
-				DigitalPortView.LENGTH,
-				when (handedness) {
-					RIGHT -> 0
-					LEFT -> -4 * SCALE
-				})
-		}
-		getPortView(model.getSourcePort())?.apply {
-			setLocation(
-			DigitalPortView.LENGTH + 4 * SCALE,
-				when (handedness) {
-					RIGHT -> SCALE
-					LEFT -> -5 * SCALE
-				})
-			direction = when (handedness) {
-				RIGHT -> SOUTH
-				LEFT -> NORTH
-			}
-		}
-		getPortView(model.getDrainPort())?.apply {
-			setLocation(
-				DigitalPortView.LENGTH + 4 * SCALE,
-				when (handedness) {
-					RIGHT -> -5 * SCALE
-					LEFT -> SCALE
-				})
-			direction = when (handedness) {
-				RIGHT -> NORTH
-				LEFT -> SOUTH
-			}
-		}
-		label.relLocation = Point2D(
-			DigitalPortView.LENGTH + WIDTH + LABEL_DIST,
-			when (handedness) {
-				RIGHT -> -2 * SCALE
-				LEFT -> 2 * SCALE
-			})
-	}
-
 	/** ---- UI properties */
 
 	var transistorType: TransistorType
@@ -259,19 +187,7 @@ class TransistorView(
 		super.drawImpl(context)
 
 		drawBody(context)
-		drawGate(context)
-		drawBulk(context)
-
-		when (handedness) {
-			RIGHT -> {
-				drawSouthSignalPort(context, getPortView(model.getSourcePort()) as DigitalPortView)
-				drawNorthSignalPort(context, getPortView(model.getDrainPort()) as DigitalPortView)
-			}
-			LEFT -> {
-				drawSouthSignalPort(context, getPortView(model.getDrainPort()) as DigitalPortView)
-				drawNorthSignalPort(context, getPortView(model.getSourcePort()) as DigitalPortView)
-			}
-		}
+		TransistorViewRenderer.Custom.render(this, context)
 	}
 
 	private fun drawLabel(context: DrawContext) {
@@ -280,6 +196,47 @@ class TransistorView(
 	}
 
 	/** ---- [TransistorView] */
+
+	private fun updateGeometry() {
+		getPortView(model.getGatePort())?.apply {
+			setLocation(
+				DigitalPortView.LENGTH,
+				when (handedness) {
+					RIGHT -> 0
+					LEFT -> -4 * SCALE
+				})
+		}
+		getPortView(model.getSourcePort())?.apply {
+			setLocation(
+				DigitalPortView.LENGTH + 4 * SCALE,
+				when (handedness) {
+					RIGHT -> SCALE
+					LEFT -> -5 * SCALE
+				})
+			direction = when (handedness) {
+				RIGHT -> SOUTH
+				LEFT -> NORTH
+			}
+		}
+		getPortView(model.getDrainPort())?.apply {
+			setLocation(
+				DigitalPortView.LENGTH + 4 * SCALE,
+				when (handedness) {
+					RIGHT -> -5 * SCALE
+					LEFT -> SCALE
+				})
+			direction = when (handedness) {
+				RIGHT -> NORTH
+				LEFT -> SOUTH
+			}
+		}
+		label.relLocation = Point2D(
+			DigitalPortView.LENGTH + WIDTH + LABEL_DIST,
+			when (handedness) {
+				RIGHT -> -2 * SCALE
+				LEFT -> 2 * SCALE
+			})
+	}
 
 	private fun updateLabel() {
 		invalidate()
@@ -304,96 +261,5 @@ class TransistorView(
 				DigitalPortView.LENGTH.toDouble(), -5.0 * SCALE,
 				WIDTH.toDouble(), HEIGHT.toDouble())
 		}
-	}
-
-	private fun drawGate(context: DrawContext) {
-		(getPortView(model.getGatePort()) as DigitalPortView).prepareConnectionDrawContext(context)
-
-		// Gate connection
-		val gateConnectionY = when(handedness) {
-			RIGHT -> 0.0
-			LEFT -> - 4.0 * SCALE
-		}
-		context.g.drawLine(
-			DigitalPortView.LENGTH.toDouble(), gateConnectionY,
-			GATE_LINE_X, gateConnectionY)
-
-		// Gate bar
-		context.g.stroke = BAR_STROKE
-		context.g.drawLine(
-			GATE_LINE_X, -4.0 * SCALE,
-			GATE_LINE_X, 0.0)
-	}
-
-	private fun drawSouthSignalPort(context: DrawContext, portView: DigitalPortView) {
-		portView.prepareConnectionDrawContext(context)
-
-		// connection
-		context.g.draw(SOUTH_SIGNAL_PATH)
-
-		// bar
-		context.g.stroke = BAR_STROKE
-		context.g.drawLine(
-			SIGNAL_LINE_X, -1.0 * SCALE,
-			SIGNAL_LINE_X, 0.0)
-	}
-
-	private fun drawNorthSignalPort(context: DrawContext, portView: DigitalPortView) {
-		portView.prepareConnectionDrawContext(context)
-
-		// connection
-		context.g.draw(NORTH_SIGNAL_PATH)
-
-		// bar
-		context.g.stroke = BAR_STROKE
-		context.g.drawLine(
-			SIGNAL_LINE_X, -4.0 * SCALE,
-			SIGNAL_LINE_X, -3.0 * SCALE)
-	}
-
-	private fun drawBulk(context: DrawContext) {
-		(getPortView(model.getDrainPort()) as DigitalPortView).prepareConnectionDrawContext(context)
-
-		val dx = if (!model.isOn && context.castedAppContext<GraphApplicationContext>()!!.isExecute) {
-			0.5 * SCALE
-		} else {
-			0.0
-		}
-
-		(getPortView(model.getDrainPort()) as DigitalPortView).prepareConnectionDrawContext(context)
-
-		context.g.translate(dx, 0.0)
-
-		// arrow
-		drawBulkArrow(context)
-
-		// bar
-		context.g.stroke = BAR_STROKE
-		context.g.drawLine(
-			SIGNAL_LINE_X, -2.5 * SCALE,
-			SIGNAL_LINE_X, -1.5 * SCALE)
-
-		context.g.translate(-dx, 0.0)
-	}
-
-	private fun drawBulkArrow(context: DrawContext) {
-		when (model.transistorType) {
-			TransistorType.P -> drawPTypeBulkArrow(context)
-			TransistorType.N -> drawNTypeBulkArrow(context)
-		}
-	}
-
-	private fun drawNTypeBulkArrow(context: DrawContext) {
-		context.g.fill(N_ARROW_PATH)
-		context.g.drawLine(
-			SIGNAL_PORT_X, -2.0 * SCALE,
-			SIGNAL_PORT_X + 1.0 * SCALE, -2.0 * SCALE)
-	}
-
-	private fun drawPTypeBulkArrow(context: DrawContext) {
-		context.g.fill(P_ARROW_PATH)
-		context.g.drawLine(
-			SIGNAL_PORT_X, -2.0 * SCALE,
-			SIGNAL_LINE_X, -2.0 * SCALE)
 	}
 }
