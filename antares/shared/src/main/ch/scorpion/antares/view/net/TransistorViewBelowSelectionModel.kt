938c1414074913dffd27af5f2f1c1293c8be8b96
Andreas Fleischmann <andreas.fleischmann@swissonline.ch>
2021-11-04 19:16:28 +0100

    more content for "Transistor" web sample project

---
+++ antares/shared/src/main/ch/scorpion/antares/view/net/TransistorViewBelowSelectionModel.kt
@@ -0,0 +1,41 @@
+package ch.scorpion.antares.view.net
+
+import ch.scorpion.antares.view.Look
+import ch.scorpion.jabbah.base.geom.Ellipse2D
+import ch.scorpion.jabbah.base.geom.RectangularShape
+import ch.scorpion.jabbah.draw.DrawContext
+import ch.scorpion.jabbah.draw.style.DrawStyleModule
+import ch.scorpion.jabbah.draw.style.StyleProvider
+import ch.scorpion.jabbah.draw.style.StyleType
+import ch.scorpion.jabbah.edit.select.AbstractBelowSelectionModel
+import ch.scorpion.jabbah.edit.style.EditStyleType
+
+class TransistorViewBelowSelectionModel(
+	transistorView: TransistorView,
+	styleProvider: StyleProvider = DrawStyleModule.styleProvider,
+	styleType: StyleType = EditStyleType.SELECTION
+) : AbstractBelowSelectionModel<TransistorView>(transistorView, styleProvider, styleType) {
+
+	private var bounds = Ellipse2D()
+
+	override val boundingBox: RectangularShape get() = bounds
+
+	override fun draw(context: DrawContext) {
+		context.g.color = color.foregroundColor
+		context.g.fill(bounds)
+	}
+
+	override fun contains(x: Double, y: Double): Boolean = bounds.contains(x, y)
+
+	override fun componentUpdated() {
+		invalidate()
+		val pos = component.location
+		bounds = Ellipse2D(
+			pos.x + 2.0 * Look.SCALE - outset,
+			pos.y -5.0 * Look.SCALE - outset,
+			TransistorView.WIDTH + 2.0 * outset,
+			TransistorView.HEIGHT + 2.0 * outset)
+		invalidate()
+		validate()
+	}
+}
