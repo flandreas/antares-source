576f5c6fecc0f8c717bf955ccdd5e24651af50a5
Andreas Fleischmann <andreas.fleischmann@swissonline.ch>
2021-11-21 08:15:02 +0100

    #245: Enable 'Open in new window' during simulation

--- graph/jvm/src/main/ch/scorpion/jabbah/graph/ui/graphviewer/NewGraphViewerAction.kt
+++ graph/jvm/src/main/ch/scorpion/jabbah/graph/ui/graphviewer/NewGraphViewerAction.kt
@@ -1,20 +1,15 @@
 package ch.scorpion.jabbah.graph.ui.graphviewer
 
+import ch.scorpion.jabbah.base.AbstractAction
 import ch.scorpion.jabbah.base.event.ActionEvent
-import ch.scorpion.jabbah.base.event.EventBus
-import ch.scorpion.jabbah.base.module.BaseModule
-import ch.scorpion.jabbah.edit.auth.Operation
-import ch.scorpion.jabbah.graph.library.AbstractContainerLibraryElementAction
 import ch.scorpion.jabbah.graph.library.ContainerLibraryElement
 import ch.scorpion.jabbah.graph.ui.library.LibraryTreeViewController
 
 class NewGraphViewerAction(
 	private val applicationName: String,
-	controller: LibraryTreeViewController
-) : AbstractContainerLibraryElementAction(
-	actionBaseName = "graph.action.newGraphViewer",
-	operation = Operation.View,
-	controller
+	private val controller: LibraryTreeViewController
+) : AbstractAction(
+	baseName = "graph.action.newGraphViewer",
 ) {
 	override fun execute(event: ActionEvent) {
 		val metaGraph = (controller.selectedItem as ContainerLibraryElement).metaGraph
