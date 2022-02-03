7918af28daf6b049b47cce4d6759095387d1afd3
Andreas Fleischmann <andreas.fleischmann@swissonline.ch>
2022-02-03 09:27:42 +0100

    fixed #297: AddressableContentsPanel didn't cooperate with Snaphot changes

---
+++ antares/jvm/src/main/ch/scorpion/antares/view/addressable/AddressableContentGraphDesktopItem.kt
@@ -0,0 +1,61 @@
+package ch.scorpion.antares.view.addressable
+
+import ch.scorpion.jabbah.app.ApplicationDataViewController
+import ch.scorpion.jabbah.draw.graphics.Color
+import ch.scorpion.jabbah.draw.graphics.CompositeColor
+import ch.scorpion.jabbah.edit.CommandManager
+import ch.scorpion.jabbah.edit.DrawingView
+import ch.scorpion.jabbah.edit.DrawingViewContent
+import ch.scorpion.jabbah.edit.module.EditModule
+import ch.scorpion.jabbah.graph.GraphApplicationContextHolder
+import ch.scorpion.jabbah.graph.ui.*
+import ch.scorpion.jabbah.graph.view.GraphView
+import java.awt.BorderLayout
+import javax.swing.JLabel
+
+/** Wraps a [AddressableContentsPanel] as a [GraphDesktopViewItem] so it can be added to the [GraphDesktopView]. */
+class AddressableContentGraphDesktopItem(
+	controller: ApplicationDataViewController,
+	addressableId: Int,
+	title: String,
+	applicationContextHolder: GraphApplicationContextHolder,
+	cmdManager: CommandManager = EditModule.commandManager,
+	readonly: Boolean = false,
+	contextColor: CompositeColor
+) : AbstractGraphDesktopItemPanel() {
+
+	private val memoryContentPanel = AddressableContentsPanel(controller, applicationContextHolder, addressableId, cmdManager, readonly)
+
+	private val headerPanel = GraphDesktopItemHeaderPanel(this, JLabel(title), allowClose = true)
+
+	init {
+		buildUI(contextColor)
+	}
+
+	private fun buildUI(contextColor: CompositeColor) {
+		layout = BorderLayout()
+		add(headerPanel, BorderLayout.NORTH)
+		add(memoryContentPanel, BorderLayout.CENTER)
+		super.contextColor = contextColor
+	}
+
+	/** ---- [GraphDesktopViewItem] */
+
+	override val drawingView: DrawingView<GraphView>? get() = null
+
+	override fun disposeItem() {
+		memoryContentPanel.dispose()
+	}
+
+	override fun findContent(condition: (DrawingViewContent<GraphView>) -> Boolean): DrawingViewContent<*>? = null
+
+	override fun addContextColorBorder(color: Color) {
+		memoryContentPanel.border = createContextColorBorder(color)
+	}
+
+	override fun removeContextColorBorder() {
+		memoryContentPanel.border = null
+	}
+
+	override fun createCloseRequest(): Any = GraphDesktopViewItemCloseRequest(this, false)
+}
