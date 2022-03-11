f9bf16fbc2450cab7933169c1790d8ac0ea91781
Andreas Fleischmann <andreas.fleischmann@swissonline.ch>
2022-03-11 15:31:20 +0100

    #322: Extracted TruthTableTableView from TruthTableDesktopItemSwing
    To make it reusable

--- antares/jvm/src/main/ch/scorpion/antares/view/truthtable/TruthTableDesktopItemSwing.kt
+++ antares/jvm/src/main/ch/scorpion/antares/view/truthtable/TruthTableDesktopItemSwing.kt
@@ -3,8 +3,6 @@ package ch.scorpion.antares.view.truthtable
 import ch.scorpion.antares.AntaresModuleJvm
 import ch.scorpion.antares.model.expression.BooleanExpressionNotation
 import ch.scorpion.antares.model.module.AntaresModelModule
-import ch.scorpion.antares.model.signal.Bit
-import ch.scorpion.antares.model.truthtable.TruthTableCommand
 import ch.scorpion.antares.model.truthtable.TruthTableLibraryItem
 import ch.scorpion.antares.model.truthtable.TruthTableReference
 import ch.scorpion.antares.model.truthtable.TruthTableService
@@ -28,32 +26,21 @@ import java.awt.BorderLayout
 import java.awt.Component
 import java.awt.Font
 import java.awt.Frame
-import java.awt.event.ActionEvent
-import java.util.*
 import javax.swing.*
 import javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED
 import javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED
-import javax.swing.table.DefaultTableCellRenderer
 import kotlin.math.max
 
 class TruthTableDesktopItemSwing(
 	private val item: TruthTableLibraryItem,
 	private val truthTableService: TruthTableService = AntaresModelModule.truthTableService,
 	private val createCircuitService: CreateCircuitFromTruthTableService = AntaresModuleJvm.createCircuitFromTruthTableService,
-	private val commandManager: CommandManager,
+	commandManager: CommandManager,
 	private val eventBus: EventBus = BaseModule.eventBus
 ) : AbstractGraphDesktopItemPanelSwing() {
 
 	companion object {
-		private const val CELL_FONT_SIZE = 18
 		private val EXPRESSION_FONT = Font(Font.MONOSPACED, Font.PLAIN, 12)
-		private const val COLUMN_WIDTH = 40
-
-		private val FOREGROUND = UIManager.getColor("TextField.foreground")
-		private val BACKGROUND = UIManager.getColor("TextField.background")
-		private val INACTIVE_FOREGROUND = UIManager.getColor("TextField.inactiveForeground")
-		private val INACTIVE_BACKGROUND = UIManager.getColor("TextField.inactiveBackground")
-
 	}
 
 	private val ref = TruthTableReference(item)
@@ -65,13 +52,7 @@ class TruthTableDesktopItemSwing(
 
 	private val closeViewRequestHandler: EventHandler<CloseViewRequest> = { handle(it) }
 
-	private val tableModel = TruthTableTableModel(ref)
-
-	private val table = JTable(tableModel)
-
-	private val scrollPane = JScrollPane(table)
-
-	private val cellFont = table.font.deriveFont(CELL_FONT_SIZE.toFloat())
+	private val tableView = TruthTableTableView(ref, commandManager)
 
 	private val generateExpressionsAction = GenerateExpressionsAction()
 
@@ -83,7 +64,6 @@ class TruthTableDesktopItemSwing(
 		eventBus.register(CloseViewRequest::class, closeViewRequestHandler)
 		buildUI()
 
-		defineSetActions()
 		createCircuitAction.enabled = false
 
 		ref.addDataListener {
@@ -102,26 +82,13 @@ class TruthTableDesktopItemSwing(
 	}
 
 	private fun createContentsPanel(): JComponent {
-		table.font = cellFont
-		table.rowHeight = CELL_FONT_SIZE + 8
-		table.autoResizeMode = JTable.AUTO_RESIZE_OFF
-		table.setShowGrid(true)
-		table.cellSelectionEnabled = true
-		table.rowMargin = 1
-		table.columnModel.columnMargin = 1
-
 		expressionsTextArea.font = EXPRESSION_FONT
-
-		scrollPane.horizontalScrollBarPolicy = HORIZONTAL_SCROLLBAR_AS_NEEDED
-		scrollPane.verticalScrollBarPolicy = VERTICAL_SCROLLBAR_AS_NEEDED
-
-		updateColumnModels()
 
 		val panel = JPanel(BorderLayout())
 		val tipLabel = JLabel(Translations.getString("library.element.truthTable.tip"))
 		tipLabel.border = BorderFactory.createEmptyBorder(5, 2, 5, 0)
 		panel.add(tipLabel, BorderLayout.NORTH)
-		panel.add(scrollPane, BorderLayout.CENTER)
+		panel.add(tableView, BorderLayout.CENTER)
 
 		return panel
 	}
@@ -180,43 +147,6 @@ class TruthTableDesktopItemSwing(
 		}
 	}
 
-	private fun defineSetActions() {
-		table.inputMap.put(KeyStroke.getKeyStroke("0"), "Action0")
-		table.actionMap.put("Action0", SetAction(Bit.False))
-		table.inputMap.put(KeyStroke.getKeyStroke("1"), "Action1")
-		table.actionMap.put("Action1", SetAction(Bit.True))
-		table.inputMap.put(KeyStroke.getKeyStroke("X"), "ActionX")
-		table.actionMap.put("ActionX", SetAction(Bit.Error))
-	}
-
-	private fun createOutputEditor(): JTextField {
-		val textField = JTextField()
-		textField.font = cellFont
-		textField.isEditable = false
-		return textField
-	}
-
-	private fun updateColumnModels() {
-		table.columnModel.columns.asSequence().forEachIndexed { index, tableColumn ->
-			if (index < ref.truthTable.inputColumnCount) {
-				tableColumn.cellRenderer = InputCellRenderer()
-			} else {
-				tableColumn.cellRenderer = OutputCellRenderer()
-				tableColumn.cellEditor = OutputCellEditor(createOutputEditor())
-			}
-			tableColumn.preferredWidth = COLUMN_WIDTH
-		}
-	}
-
-	private fun applyZebra(label: JLabel, row: Int, output: Boolean) {
-		label.foreground = if (output) FOREGROUND else INACTIVE_FOREGROUND
-		if (row.and(4) == 0) {
-			label.background = BACKGROUND
-		} else {
-			label.background = INACTIVE_BACKGROUND
-		}
-	}
-
 	private fun generateExpressions() {
 		expressionsTextArea.text = truthTableService.generateExpressions(
 			ref.truthTable,
@@ -229,70 +159,6 @@ class TruthTableDesktopItemSwing(
 			Frame.getFrames()[0],
 			item,
 			createCircuitService)
-	}
-
-	private inner class InputCellRenderer : DefaultTableCellRenderer() {
-
-		override fun getTableCellRendererComponent(table: JTable?, value: Any?, isSelected: Boolean, hasFocus: Boolean, row: Int, column: Int): Component {
-			val label = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column) as JLabel
-			label.horizontalAlignment = SwingConstants.CENTER
-			label.verticalAlignment = SwingConstants.CENTER
-			label.font = cellFont
-
-			if (!isSelected && !hasFocus) {
-				applyZebra(label, row, output = false)
-			}
-
-			return label
-		}
-	}
-
-	private inner class OutputCellRenderer : DefaultTableCellRenderer() {
-
-		override fun getTableCellRendererComponent(table: JTable?, value: Any?, isSelected: Boolean, hasFocus: Boolean, row: Int, column: Int): Component {
-			val label = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column) as JLabel
-			label.horizontalAlignment = SwingConstants.CENTER
-			label.font = cellFont
-
-			if (!isSelected && !hasFocus) {
-				applyZebra(label, row, output = true)
-			}
-
-			return label
-		}
-	}
-
-	private inner class OutputCellEditor(textField: JTextField) : DefaultCellEditor(textField) {
-		override fun isCellEditable(anEvent: EventObject?): Boolean = false
-	}
-
-	private inner class SetAction(private val bit: Bit) : AbstractAction() {
-
-		override fun actionPerformed(e: ActionEvent?) {
-			val row = table.selectedRow
-			var column = table.selectedColumn
-
-			if (column in ref.truthTable.inputColumnCount until table.columnCount) {
-				commandManager.execute(TruthTableCommand(ref, row, column, bit))
-			}
-
-			forwardSelection(row, column)
-		}
-
-		private fun forwardSelection(row: Int, column: Int) {
-			var r = row
-			var c = column
-
-			c++
-			if (c >= table.columnCount) {
-				c = ref.truthTable.inputColumnCount
-				r++
-				if (r >= ref.truthTable.rowsCount) {
-					r = 0
-				}
-			}
-			table.changeSelection(r, c, false, false)
-		}
 	}
 
 	private inner class GenerateExpressionsAction
