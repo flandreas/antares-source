b00b291019a74f58e99b0b510cafaac553d41fe3
Andreas Fleischmann <andreas.fleischmann@swissonline.ch>
2022-05-18 14:13:23 +0200

    #374: Introduced GraphNameAbbreviator

---
+++ graph/shared/src/main/ch/scorpion/jabbah/graph/model/graph/GraphNameAbbreviator.kt
@@ -0,0 +1,30 @@
+package ch.scorpion.jabbah.graph.model.graph
+
+import ch.scorpion.jabbah.graph.model.Graph
+
+/**
+ * Abbreviates the name of a [Graph] such that it is short enough to be
+ * placed within a narrow symbol.
+ */
+object GraphNameAbbreviator {
+
+	private const val LENGTH = 3
+
+	fun abbreviate(name: String): String {
+		val effName = name.filterNot { it.isWhitespace() }
+
+		if (effName.length <= LENGTH) {
+			return effName
+		}
+
+		if (name.contains(' ')) {
+			return name
+				.split(' ')
+				.map { it.filterNot { c -> c.isWhitespace() } }
+				.joinToString("") { it.firstOrNull()?.uppercase() ?: "" }
+				.take(LENGTH)
+		}
+
+		return effName.take(LENGTH).uppercase()
+	}
+}
