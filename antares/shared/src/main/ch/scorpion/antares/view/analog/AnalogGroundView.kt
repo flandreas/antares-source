39821d5083d834d8475cfd2c330f68a436dcf270
Andreas Fleischmann <flandreas@icloud.com>
2023-01-26 12:28:17 +0100

    Analog: Draw line in AnalogGroundView before triangle

--- antares/shared/src/main/ch/scorpion/antares/view/analog/AnalogGroundView.kt
+++ antares/shared/src/main/ch/scorpion/antares/view/analog/AnalogGroundView.kt
@@ -48,13 +48,14 @@ class AnalogGroundView(
 			}
 		}
 
+		context.g.color = context.chooseForeground(foregroundColor)
+		(getPortView(model.getPort()) as AbstractAntaresPortView).prepareConnectionDrawContext(context)
+		context.g.drawLine(0.0, LENGTH.toDouble(), 0.0, h(3.5))
+
 		context.g.color = context.chooseBackground(backgroundColor)
 		context.g.fill(TRIANGLE)
 		context.g.color = context.chooseForeground(foregroundColor)
 		context.g.stroke = stroke
 		context.g.draw(TRIANGLE)
-
-		(getPortView(model.getPort()) as AbstractAntaresPortView).prepareConnectionDrawContext(context)
-		context.g.drawLine(0.0, LENGTH.toDouble(), 0.0, h(3.5))
 	}
 }
