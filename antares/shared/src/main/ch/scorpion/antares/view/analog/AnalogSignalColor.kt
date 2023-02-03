bdab6e2a62cf02669e05ca150c50f5a8e63c8c83
Andreas Fleischmann <flandreas@icloud.com>
2023-02-03 07:20:05 +0100

    Analog: Draw neg. voltage in red color

--- antares/shared/src/main/ch/scorpion/antares/view/analog/AnalogSignalColor.kt
+++ antares/shared/src/main/ch/scorpion/antares/view/analog/AnalogSignalColor.kt
@@ -2,23 +2,36 @@ package ch.scorpion.antares.view.analog
 
 import ch.scorpion.antares.model.analog.AnalogSignal
 import ch.scorpion.antares.view.style.AntaresTheme
+import ch.scorpion.jabbah.draw.graphics.Color
 import ch.scorpion.jabbah.draw.graphics.CompositeColor
 import ch.scorpion.jabbah.draw.graphics.CompositeColorGradient
 import ch.scorpion.jabbah.draw.style.Themes
+import kotlin.math.absoluteValue
 
 /**
  * Determines the [CompositeColor] in which an [AnalogSignal] is to be drawn during simulation.
  */
 object AnalogSignalColor {
 
-	private val GRADIENT = CompositeColorGradient(
+	private val MAX_NEG_COLOR = Color(247, 76, 16)
+
+	private val POS_GRADIENT = CompositeColorGradient(
 		Themes.get<AntaresTheme>().one.backgroundColor,
 		Themes.get<AntaresTheme>().zero.foregroundColor,
 		Themes.get<AntaresTheme>().one.foregroundColor)
+
+	private val NEG_GRADIENT = CompositeColorGradient(
+		Themes.get<AntaresTheme>().one.backgroundColor,
+		Themes.get<AntaresTheme>().zero.foregroundColor,
+		MAX_NEG_COLOR)
 
 	private const val MIN_VOLTAGE = 0.0
 	private const val MAX_VOLTAGE = 5.0
 
 	fun ofSignal(signal: AnalogSignal): CompositeColor =
-		GRADIENT.at((signal.voltage.coerceIn(MIN_VOLTAGE, MAX_VOLTAGE) / MAX_VOLTAGE).toFloat())
+		if (signal.voltage >= 0) {
+			POS_GRADIENT.at((signal.voltage.coerceIn(MIN_VOLTAGE, MAX_VOLTAGE) / MAX_VOLTAGE).toFloat())
+		} else {
+			NEG_GRADIENT.at((signal.voltage.absoluteValue.coerceAtMost(MAX_VOLTAGE) / MAX_VOLTAGE).toFloat())
+		}
 }
