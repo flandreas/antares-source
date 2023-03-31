2a9fd8be6bb7acbc656277d45234dcb9cd78a148
Andreas Fleischmann <flandreas@icloud.com>
2023-03-31 22:06:04 +0200

    #547: Usability of disabled "New Circuit" action

--- graph/jvm/src/main/ch/scorpion/jabbah/graph/library/NewGraphAction.kt
+++ graph/jvm/src/main/ch/scorpion/jabbah/graph/library/NewGraphAction.kt
@@ -1,25 +1,29 @@
 package ch.scorpion.jabbah.graph.library
 
+import ch.scorpion.jabbah.base.AbstractAction
 import ch.scorpion.jabbah.base.StringUtils
 import ch.scorpion.jabbah.base.Translations
+import ch.scorpion.jabbah.base.event.EventBus
 import ch.scorpion.jabbah.base.logger
-import ch.scorpion.jabbah.edit.auth.Authorizer
+import ch.scorpion.jabbah.base.module.BaseModule
 import ch.scorpion.jabbah.edit.auth.Operation
+import ch.scorpion.jabbah.edit.module.EditModule
 import ch.scorpion.jabbah.graph.MetaGraph
 import ch.scorpion.jabbah.graph.ui.library.LibraryTreeViewController
+import java.awt.Frame
 import javax.swing.JComponent
 import javax.swing.JOptionPane
 import javax.swing.SwingUtilities
 
 /**
- * Creates a new [ContainerLibraryElement] with an empty [MetaGraph] as a child of the currently selected [LibraryDirectory].
+ * Creates a new [ContainerLibraryElement] with an empty [MetaGraph] as a child of the
+ * currently selected [LibraryDirectory].
  */
 class NewGraphAction(
-	controller: LibraryTreeViewController,
-) : AbstractLibraryFolderAction(
-	actionBaseName = "library.action.newGraph",
-	operation = Operation.Change,
-	controller
+	private val controller: LibraryTreeViewController,
+	private val eventBus: EventBus = BaseModule.eventBus
+) : AbstractAction(
+	baseName = "library.action.newGraph"
 ) {
 
 	companion object {
@@ -45,28 +49,53 @@ class NewGraphAction(
 		}
 	}
 
-	private val operationTarget: () -> Any? get() = {
-		if (selectedItem is LibraryDirectory) {
-			selectedFolder.library
-		} else {
-			null
-		}
+	init {
+		// Always enabled, check enabledness on execution in order to show information dialog
+		enabled = true
 	}
 
     override fun execute(event: ch.scorpion.jabbah.base.event.ActionEvent) {
-	    val newName = requestNewGraphName(controller.view as JComponent, name)
-		    ?: return
+	    if (checkEnabledness()) {
+		    val newName = requestNewGraphName(controller.view as JComponent, name)
+			    ?: return
 
-	    LOG.info("$name '$newName'")
+		    LOG.info("$name '$newName'")
 
-        val directory = controller.selectedItem as LibraryDirectory
-	    val library = directory.library!!
-	    val metaGraph = MetaGraph.withName(newName)
+		    val directory = controller.selectedItem as LibraryDirectory
+		    val library = directory.library!!
+		    val metaGraph = MetaGraph.withName(newName)
 
-	    val element = library.libraryService.addContainerLibraryElement(library, metaGraph, directory)
-		eventBus.post(OpenContainerLibraryElementRequest(element))
+		    val element = library.libraryService.addContainerLibraryElement(library, metaGraph, directory)
+		    eventBus.post(OpenContainerLibraryElementRequest(element))
+	    }
     }
 
-	override val operationAuthorized: Boolean
-		get() = operationTarget.invoke() != null && Authorizer.isCurrentUserAuthorizedTo(operation, operationTarget.invoke()!!)
+	private fun checkEnabledness(): Boolean {
+		getEnablednessProhibitionKey()?.let {
+			JOptionPane.showMessageDialog(
+				Frame.getFrames()[0],
+				Translations.getString(it),
+				Translations.getString("library.action.newGraph.name"),
+				JOptionPane.ERROR_MESSAGE)
+			return false
+		}
+		return true
+	}
+
+	private fun getEnablednessProhibitionKey(): String? {
+		if (controller.applicationModeHolder.currentMode.isExecute()) {
+			return "library.action.newGraph.notEditMode.error"
+		}
+		if (EditModule.commandManager.canUndo()) {
+			return "library.action.newGraph.unsavedChanges.error"
+		}
+		if (controller.selectedItem !is LibraryDirectory) {
+			return "library.action.newGraph.noDirectory.error"
+		}
+		if (!AbstractLibraryAction.isAuthorized(Operation.Change, controller.selectedItem?.library)) {
+			return "library.action.newGraph.notAuthorized.error"
+		}
+
+		return null
+	}
 }
