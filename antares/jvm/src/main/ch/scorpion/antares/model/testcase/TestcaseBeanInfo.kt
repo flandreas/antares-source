eac36c4e6ceafe92b3339b3645a00f45b69ae01c
Andreas Fleischmann <flandreas@icloud.com>
2023-10-26 19:29:23 +0200

    #657: Circuit tests - Fixed editing testcase name

--- antares/jvm/src/main/ch/scorpion/antares/model/testcase/TestcaseBeanInfo.kt
+++ antares/jvm/src/main/ch/scorpion/antares/model/testcase/TestcaseBeanInfo.kt
@@ -1,7 +1,6 @@
 package ch.scorpion.antares.model.testcase
 
 import ch.scorpion.antares.model.DigitalGraph
-import ch.scorpion.antares.model.testcase.Testcase
 import ch.scorpion.jabbah.edit.Bean
 import ch.scorpion.jabbah.edit.BeanProvider
 import ch.scorpion.jabbah.edit.Editor
@@ -19,8 +18,8 @@ class TestcaseBeanInfo : AbstractBeanInfo<Testcase>() {
 			listOf(((e.drawing as GraphView).graph as DigitalGraph).testcases.get(ids.iterator().next().toInt()) as Bean)
 		}
 
-		private val name = EditProperties.name()
-		private val description = EditProperties.description()
+		private val name = EditProperties.name(beanProvider = testcaseBeanProvider)
+		private val description = EditProperties.description(beanProvider = testcaseBeanProvider)
 	}
 
 	override fun addProperties(bean: Testcase, editor: Editor, properties: MutableList<Property>) {
