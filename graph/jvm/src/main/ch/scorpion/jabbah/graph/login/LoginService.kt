7bf7dfa56c7bbcfaa0752bfb839760912dd7297a
Andreas Fleischmann <flandreas@icloud.com>
2024-02-15 14:48:43 +0100

    #684: Web - Changed LoginResponse structure

--- graph/jvm/src/main/ch/scorpion/jabbah/graph/login/LoginService.kt
+++ graph/jvm/src/main/ch/scorpion/jabbah/graph/login/LoginService.kt
@@ -2,9 +2,11 @@ package ch.scorpion.jabbah.graph.login
 
 import ch.scorpion.jabbah.base.net.httpClient
 import ch.scorpion.jabbah.base.logger
+import io.ktor.client.call.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
+import kotlinx.serialization.Serializable
 import java.net.URL
 
 interface LoginService {
@@ -31,8 +33,8 @@ class LoginServiceJvm(
             return false
         }
 
-        val token = response.bodyAsText()
-        Session.establish(SessionData(request.username, token))
+        val loginResponse = response.body<LoginResponse>()
+        Session.establish(SessionData(request.username, loginResponse.token))
 
         LOG.info("Successfully logged in to Akrab")
 
@@ -43,4 +45,9 @@ class LoginServiceJvm(
         LOG.userTrail("Logout")
         Session.drop()
     }
+
+    @Serializable
+    private data class LoginResponse(
+        val token: String
+    )
 }
