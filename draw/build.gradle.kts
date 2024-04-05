c26cb326d3c54b5b2e033d29b38ac99f6f1999ca
Andreas Fleischmann <flandreas@icloud.com>
2024-04-05 08:51:24 +0200

    Removed demo package from draw.jvm module

--- draw/build.gradle.kts
+++ draw/build.gradle.kts
@@ -1,53 +1,9 @@
-import org.jetbrains.kotlin.gradle.dsl.KotlinJvmOptions
-
-val mockkVersion: String by extra
-val slf4jVersion: String by extra
-val commonsIoVersion: String by extra
-val commonsLang3Version: String by extra
-val l2fprodVersion: String by extra
-
-val batikVersion = "1.17"
-
 kotlin {
-
 	sourceSets {
-
 		val commonMain by getting {
 			dependencies {
 				implementation(project(":base"))
 				implementation(project(":animation"))
-			}
-		}
-
-		val commonDemo by creating {
-			kotlin.srcDir("shared/src/demo")
-			dependencies {
-				dependsOn(commonMain)
-			}
-		}
-
-		val jvmMain by getting
-
-		targets.getByName("jvm").compilations {
-			val main by getting
-			val demo by creating {
-				kotlinOptions {
-					(this as KotlinJvmOptions).jvmTarget = JavaVersion.VERSION_1_8.toString()
-				}
-				defaultSourceSet {
-					kotlin.srcDir("jvm/src/demo")
-					dependencies {
-						dependsOn(commonMain)
-						dependsOn(commonDemo)
-						dependsOn(jvmMain)
-						implementation(main.compileDependencyFiles + main.output.classesDirs)
-					}
-				}
-			}
-
-			tasks.create<JavaExec>("demo") {
-				setMain("ch.scorpion.jabbah.draw.hellographics.HelloGraphicsJvm")
-				classpath = demo.output.classesDirs + main.compileDependencyFiles + commonMain.resources.sourceDirectories
 			}
 		}
 	}
