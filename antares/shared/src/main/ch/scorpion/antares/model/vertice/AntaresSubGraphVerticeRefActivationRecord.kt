abe0826da991db3b6ec57595b59ad8918849aa45
Andreas Fleischmann <flandreas@icloud.com>
2024-08-18 18:16:01 +0200

    #801: Error in DSL when using Int in analog circuit script

--- antares/shared/src/main/ch/scorpion/antares/model/vertice/AntaresSubGraphVerticeRefActivationRecord.kt
+++ antares/shared/src/main/ch/scorpion/antares/model/vertice/AntaresSubGraphVerticeRefActivationRecord.kt
@@ -1,5 +1,6 @@
 package ch.scorpion.antares.model.vertice
 
+import ch.scorpion.antares.model.AntaresGraphTypes
 import ch.scorpion.antares.model.port.DigitalPort
 import ch.scorpion.antares.model.signal.DigitalSignal
 import ch.scorpion.antares.model.signal.DigitalSignalFactory
@@ -7,6 +8,7 @@ import ch.scorpion.antares.model.analog.AnalogSignal
 import ch.scorpion.jabbah.base.Translations
 import ch.scorpion.jabbah.base.dsl.RuntimeError
 import ch.scorpion.jabbah.base.dsl.Variable
+import ch.scorpion.jabbah.base.logger
 import ch.scorpion.jabbah.execution.SignalHandler
 import ch.scorpion.jabbah.graph.model.GraphPort
 import ch.scorpion.jabbah.graph.model.vertice.SubGraphVerticeRef
@@ -21,12 +23,23 @@ class AntaresSubGraphVerticeRefActivationRecord(
 	signalHandler: SignalHandler
 ) : SubGraphVerticeRefActivationRecord(verticeRef, signalHandler) {
 
+	companion object {
+		private val LOG by logger(AntaresSubGraphVerticeRefActivationRecord::class)
+	}
+
 	override fun setValue(variable: Variable, value: Any) {
 		val port = verticeRef.getPort<DigitalSignal>(variable.token.value!!) as DigitalPort
 		val effValue = when (value) {
 			is DigitalSignal -> value
 			is AnalogSignal -> value
-			is Long -> DigitalSignalFactory.of(port.bitWidth, value)
+			is Long -> when (verticeRef.getGraphIfPresent()?.type) {
+				AntaresGraphTypes.Analog -> AnalogSignal(value.toDouble())
+				AntaresGraphTypes.Digital -> DigitalSignalFactory.of(port.bitWidth, value)
+				else -> {
+					LOG.error("Unsupported GraphType ${verticeRef.graphType::class.simpleName}")
+					throw RuntimeError(variable.location, "Unsupported GraphType")
+				}
+			}
 			is Float -> AnalogSignal(value.toDouble())
 			else -> throw RuntimeError(variable.location, Translations.getString("graph.dsl.cannotSetPortValue.msg"))
 		}
