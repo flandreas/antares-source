b5b03673cdc30678078badb38af9974b99e1131a
Andreas Fleischmann <flandreas@icloud.com>
2024-08-22 15:32:22 +0200

    Added "Description" property to AnalogCircuitInOutViewBeanInfo

--- antares/jvm/src/main/ch/scorpion/antares/view/analog/AnalogCircuitInOutViewBeanInfo.kt
+++ antares/jvm/src/main/ch/scorpion/antares/view/analog/AnalogCircuitInOutViewBeanInfo.kt
@@ -1,6 +1,18 @@
 package ch.scorpion.antares.view.analog
 
 import ch.scorpion.antares.view.inout.AbstractCircuitInOutViewBeanInfo
+import ch.scorpion.jabbah.edit.Editor
+import ch.scorpion.jabbah.edit.model.EditProperties
+import com.l2fprod.common.propertysheet.Property
 
 @Suppress("unused")
-class AnalogCircuitInOutViewBeanInfo : AbstractCircuitInOutViewBeanInfo<AnalogCircuitInOutView>()
+class AnalogCircuitInOutViewBeanInfo : AbstractCircuitInOutViewBeanInfo<AnalogCircuitInOutView>() {
+    companion object {
+        private val description = EditProperties.description()
+    }
+
+    override fun addProperties(bean: AnalogCircuitInOutView, editor: Editor, properties: MutableList<Property>) {
+        super.addProperties(bean, editor, properties)
+        properties.add(description.bind(editor, beanIdProvider(bean.id)))
+    }
+}
