bf22c9771f35a662b489570c6a188c0fc993aa0a
Andreas Fleischmann <flandreas@icloud.com>
2025-12-06 08:45:44 +0100

    Adjusted LightColorRenderer for dark mode

--- antares/jvm/src/main/ch/scorpion/antares/view/AntaresPropertyUIs.kt
+++ antares/jvm/src/main/ch/scorpion/antares/view/AntaresPropertyUIs.kt
@@ -14,44 +14,14 @@ import ch.scorpion.antares.view.input.JoystickDeflection
 import ch.scorpion.antares.view.net.TransistorViewSymbol
 import ch.scorpion.antares.view.net.tunnel.TunnelFlowDirection
 import ch.scorpion.antares.view.output.LightColor
-import ch.scorpion.antares.view.output.LightColorExpression
 import ch.scorpion.antares.view.output.VideoRamColorModel
 import ch.scorpion.antares.view.port.DigitalPortViewStyle
 import ch.scorpion.jabbah.base.Translations
 import ch.scorpion.jabbah.base.sound.WaveformType
-import ch.scorpion.jabbah.base.swing.ColorIcon
 import ch.scorpion.jabbah.base.swing.EnumRenderer
 import ch.scorpion.jabbah.base.swing.ToStringRenderer
-import ch.scorpion.jabbah.draw.graphics.Graphics2DJvm
 import com.l2fprod.common.beans.editor.ComboBoxPropertyEditor
 import javax.swing.JComboBox
-import javax.swing.JList
-
-/**
- * Renders a [LightColor] in a [JList].
- */
-class LightColorRenderer : EnumRenderer<LightColor>() {
-
-    private val icon = ColorIcon()
-
-    override fun setValue(value: Any?) {
-	    if (value == null) {
-			icon.backgroundColor = Graphics2DJvm.toAwtColor(LightColor.getSystemDefault().executeColor(true))
-			text = Translations.getString("element.color.none")
-			setIcon(icon)
-		} else if (value is LightColorExpression) {
-			text = value.toString()
-			setIcon(null)
-	    } else if (value is LightColor) {
-		    icon.backgroundColor = Graphics2DJvm.toAwtColor(value.executeColor(true))
-		    text = value.toString()
-			setIcon(icon)
-	    } else {
-			text = value.toString()
-			setIcon(null)
-		}
-    }
-}
 
 class LightColorEditor(optional: Boolean = false) : ComboBoxPropertyEditor() {
     init {
---
+++ antares/jvm/src/main/ch/scorpion/antares/view/LightColorRenderer.kt
@@ -0,0 +1,49 @@
+package ch.scorpion.antares.view
+
+import ch.scorpion.antares.view.output.LightColor
+import ch.scorpion.antares.view.output.LightColorExpression
+import ch.scorpion.jabbah.base.Translations
+import ch.scorpion.jabbah.base.swing.ColorIcon
+import ch.scorpion.jabbah.base.swing.EnumRenderer
+import ch.scorpion.jabbah.base.ui.UI
+import ch.scorpion.jabbah.draw.graphics.Graphics2DJvm
+import java.awt.Color
+
+class LightColorRenderer : EnumRenderer<LightColor>() {
+
+    private val icon = ColorIcon()
+
+    private val colorCache = mutableMapOf<LightColor, Color>()
+
+    private fun getColor(lightColor: LightColor): Color =
+        colorCache.computeIfAbsent(lightColor) {
+            var color = lightColor.executeColor(true)
+            if (UI.isDark) {
+                color = color.darker()
+            }
+            Graphics2DJvm.toAwtColor(color)
+        }
+
+    override fun setValue(value: Any?) {
+        when (value) {
+            null -> {
+                icon.backgroundColor = Graphics2DJvm.toAwtColor(LightColor.getSystemDefault().executeColor(true))
+                text = Translations.getString("element.color.none")
+                setIcon(icon)
+            }
+            is LightColorExpression -> {
+                text = value.toString()
+                setIcon(null)
+            }
+            is LightColor -> {
+                icon.backgroundColor = getColor(value)
+                text = value.toString()
+                setIcon(icon)
+            }
+            else -> {
+                text = value.toString()
+                setIcon(null)
+            }
+        }
+    }
+}
