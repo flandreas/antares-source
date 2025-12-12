663bc6b8f64a11f1a6e61751382db568083ce05b
Andreas Fleischmann <flandreas@icloud.com>
2025-12-12 07:51:37 +0100

    #1091: Request saving changes before finding unconnected ports

--- graph/jvm/src/main/ch/scorpion/jabbah/graph/view/connect/unconnected/FindUnconnectedPortsAction.kt
+++ graph/jvm/src/main/ch/scorpion/jabbah/graph/view/connect/unconnected/FindUnconnectedPortsAction.kt
@@ -1,10 +1,14 @@
 package ch.scorpion.jabbah.graph.view.connect.unconnected
 
+import ch.scorpion.jabbah.base.Translations
 import ch.scorpion.jabbah.base.event.ActionEvent
 import ch.scorpion.jabbah.base.invocation.InvocationHandler
+import ch.scorpion.jabbah.edit.module.EditModule
 import ch.scorpion.jabbah.graph.library.ContainerLibraryElement
 import ch.scorpion.jabbah.graph.library.LibraryModule
 import ch.scorpion.jabbah.graph.ui.library.LibraryTreeViewController
+import java.awt.Frame
+import javax.swing.JOptionPane
 
 class FindUnconnectedPortsAction(
     controller: LibraryTreeViewController
@@ -13,6 +17,10 @@ class FindUnconnectedPortsAction(
     override val opensDialog: Boolean get() = true
 
     override fun execute(event: ActionEvent) {
+        if (unsavedChanges()) {
+            return
+        }
+
         val type = FindUnconnectedPortsPanel.showAsDialog()
         if (type != null) {
             val metaGraph = LibraryModule.libraryHolder.library.getMetaGraph((selectedItem as ContainerLibraryElement).uuid)
@@ -26,5 +34,19 @@ class FindUnconnectedPortsAction(
                 }
             }
         }
+    }
+
+    private fun unsavedChanges(): Boolean {
+        if (EditModule.commandManager.canUndo()) {
+            JOptionPane.showMessageDialog(
+                Frame.getFrames()[0],
+                Translations.getString("graph.action.findUnconnectedPorts.unsavedChanged"),
+                name,
+                JOptionPane.ERROR_MESSAGE
+            )
+            return true
+        }
+
+        return false
     }
 }
