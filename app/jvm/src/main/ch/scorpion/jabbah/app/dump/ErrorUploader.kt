77685f7ea8916fec76871a33856a571da4a0776a
Andreas Fleischmann <flandreas@icloud.com>
2025-12-24 14:44:31 +0100

    Avoid creating repetitive uploading error dumps already in ErrorUploader
    Before dump is created and suspended method with check gets called

--- app/jvm/src/main/ch/scorpion/jabbah/app/dump/ErrorUploader.kt
+++ app/jvm/src/main/ch/scorpion/jabbah/app/dump/ErrorUploader.kt
@@ -3,8 +3,12 @@ package ch.scorpion.jabbah.app.dump
 import ch.scorpion.jabbah.app.DesktopApplication
 import ch.scorpion.jabbah.base.invocation.ErrorHandlerPlugin
 import ch.scorpion.jabbah.base.invocation.InteractiveErrorHandler
+import java.time.LocalDateTime
 
 object ErrorUploader : AbstractDumpCreator(), ErrorHandlerPlugin {
+
+    /** The time when the last unexpected error occurred. Used to avoid uploading the same error multiple times. */
+    private var lastDateTime: LocalDateTime? = null
 
     override fun initialize(application: DesktopApplication) {
         super.initialize(application)
@@ -12,6 +16,11 @@ object ErrorUploader : AbstractDumpCreator(), ErrorHandlerPlugin {
     }
 
     override fun handleError(t: Throwable) {
+        if (lastDateTime != null) {
+            return
+        }
+        lastDateTime = LocalDateTime.now()
+
         uploadErrorDump(t.message ?: "", false)
     }
 }
