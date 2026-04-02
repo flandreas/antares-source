bd660051453f9d8aca76410082388b79e2ea6146
Andreas Fleischmann <flandreas@icloud.com>
2026-04-02 09:14:07 +0200

    Improved source docu in VerticeLink

--- graph/shared/src/main/ch/scorpion/jabbah/graph/model/vertice/VerticeLink.kt
+++ graph/shared/src/main/ch/scorpion/jabbah/graph/model/vertice/VerticeLink.kt
@@ -1,11 +1,18 @@
 package ch.scorpion.jabbah.graph.model.vertice
 
+import ch.scorpion.jabbah.io.Storable
+import ch.scorpion.jabbah.io.ReferenceResolver
 import ch.scorpion.jabbah.graph.model.Graph
 import ch.scorpion.jabbah.graph.model.Vertice
+import ch.scorpion.jabbah.graph.model.GraphElement
 
 /**
  * A link to object supporting late binding. Used in conjunction with undo/redo, which must avoid
  * object references, because those can change due to undo/redo snapshot exchanges.
+ *
+ * Because [VerticeLink]s are dereferenced (i.e. accessing the linked object) only during [GraphElement.bind]
+ * and NOT while being loaded as [Storable], [VerticeLink] uses the real [GraphElement.id] for references
+ * and not the global IDs "_id" used by [ReferenceResolver] during reading from persistent store.
  */
 interface ObjectLink<out T: Any> {
 
